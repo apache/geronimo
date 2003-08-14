04523ca13d3fc34d9840c635cc520144e9602525
Jason Dillon <jdillon@apache.org>
2003-08-14 16:14:32 +0000

    o Moved catch-all J2EE spec sources from core
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@43796 13f79535-47bb-0310-9956-ffa450edef68

---
+++ specs/j2ee/src/java/javax/transaction/xa/XAResource.java
@@ -0,0 +1,88 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ *    "Apache Geronimo" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    "Apache Geronimo", nor may "Apache" appear in their name, without
+ *    prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * This source code implements specifications defined by the Java
+ * Community Process. In order to remain compliant with the specification
+ * DO NOT add / change / or delete method signatures!
+ *
+ * ====================================================================
+ */
+package javax.transaction.xa;
+/**
+ *
+ *
+ *
+ * @version $Revision: 1.1 $ $Date: 2003/08/14 16:14:32 $
+ */
+public interface XAResource {
+    int TMENDRSCAN = 8388608;
+    int TMFAIL = 536870912;
+    int TMJOIN = 2097152;
+    int TMNOFLAGS = 0;
+    int TMONEPHASE = 1073741824;
+    int TMRESUME = 134217728;
+    int TMSTARTRSCAN = 16777216;
+    int TMSUCCESS = 67108864;
+    int TMSUSPEND = 33554432;
+    int XA_RDONLY = 3;
+    int XA_OK = 0;
+    void commit(Xid xid, boolean b) throws XAException;
+    void end(Xid xid, int i) throws XAException;
+    void forget(Xid xid) throws XAException;
+    int getTransactionTimeout() throws XAException;
+    boolean isSameRM(XAResource xaResource) throws XAException;
+    int prepare(Xid xid) throws XAException;
+    Xid[] recover(int i) throws XAException;
+    void rollback(Xid xid) throws XAException;
+    boolean setTransactionTimeout(int i) throws XAException;
+    void start(Xid xid, int i) throws XAException;}
