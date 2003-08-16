34eb99c1980707dbf70899d3182b1bee7386a7d5
Bruce Snyder <bsnyder@apache.org>
2003-08-16 05:26:50 +0000

    Adding J2EE Connector Architecture submitted by Maas van den Berg <email@dds.nl>.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@43831 13f79535-47bb-0310-9956-ffa450edef68

---
+++ specs/j2ee-connector/src/java/javax/resource/spi/security/PasswordCredential.java
@@ -0,0 +1,120 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ *    "Apache Geronimo" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    "Apache Geronimo", nor may "Apache" appear in their name, without
+ *    prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * ====================================================================
+ */
+
+//
+// This source code implements specifications defined by the Java
+// Community Process. In order to remain compliant with the specification
+// DO NOT add / change / or delete method signatures!
+//
+package javax.resource.spi.security;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import javax.resource.spi.ManagedConnectionFactory;
+
+/**
+ *
+ *
+ *
+ * @version $Revision: 1.1 $ $Date: 2003/08/16 05:22:10 $
+ */
+public final class PasswordCredential implements Serializable {
+    private String userName;
+    private char[] password;
+    private ManagedConnectionFactory mcf;
+
+    public PasswordCredential(String userName, char[] password) {
+        this.userName = userName;
+        this.password = (char[]) password.clone();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public char[] getPassword() {
+        return (char[]) password.clone();
+    }
+
+    public ManagedConnectionFactory getManagedConnectionFactory() {
+        return mcf;
+    }
+
+    public void setManagedConnectionFactory(ManagedConnectionFactory mcf) {
+        this.mcf = mcf;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PasswordCredential)) return false;
+
+        final PasswordCredential credential = (PasswordCredential) o;
+
+        if (!Arrays.equals(password, credential.password)) return false;
+        if (!userName.equals(credential.userName)) return false;
+
+        return true;
+    }
+
+    public int hashCode() {
+        //@todo fix this
+        int result = userName.hashCode();
+        for (int i=0; i<password.length; i++) {
+            result *= password[i];
+        }
+        return result;
+    }
+}
