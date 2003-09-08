7b83cf6f6f528fab5b82993b9169861c4659688e
Dain Sundstrom <dain@apache.org>
2003-09-08 04:29:53 +0000

    Updated to reflect movement of base services code from o.a.g.common in the core module to o.a.g.core.service in the core module.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@44256 13f79535-47bb-0310-9956-ffa450edef68

--- modules/core/src/java/org/apache/geronimo/naming/java/ComponentContextInterceptor.java
+++ modules/core/src/java/org/apache/geronimo/naming/java/ComponentContextInterceptor.java
@@ -55,15 +55,15 @@
  */
 package org.apache.geronimo.naming.java;
 
-import org.apache.geronimo.common.AbstractInterceptor;
-import org.apache.geronimo.common.Invocation;
-import org.apache.geronimo.common.InvocationResult;
+import org.apache.geronimo.core.service.AbstractInterceptor;
+import org.apache.geronimo.core.service.Invocation;
+import org.apache.geronimo.core.service.InvocationResult;
 
 /**
  * An interceptor that pushes the current component's java:comp context into
  * the java: JNDI namespace
  *
- * @version $Revision: 1.3 $ $Date: 2003/09/04 05:16:17 $
+ * @version $Revision: 1.4 $ $Date: 2003/09/08 04:29:53 $
  */
 public class ComponentContextInterceptor extends AbstractInterceptor {
     private final ReadOnlyContext compContext;
