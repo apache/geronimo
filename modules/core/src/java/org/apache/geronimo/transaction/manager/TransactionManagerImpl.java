520ca5815d0465a8a9f1a8415ddff8248c681b7f
Jeremy Boynes <jboynes@apache.org>
2003-09-29 00:32:40 +0000

    Simple impl of JTA TransactionManager
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@44319 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/core/src/java/org/apache/geronimo/transaction/manager/TransactionManagerImpl.java
@@ -0,0 +1,149 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ *    "Apache Geronimo" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    "Apache Geronimo", nor may "Apache" appear in their name, without
+ *    prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * ====================================================================
+ */
+package org.apache.geronimo.transaction.manager;
+
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.InvalidTransactionException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.Status;
+import javax.transaction.SystemException;
+import javax.transaction.Transaction;
+import javax.transaction.TransactionManager;
+
+import org.apache.geronimo.transaction.log.UnrecoverableLog;
+
+/**
+ * Simple implementation of a local transaction manager.
+ *
+ * @version $Revision: 1.1 $ $Date: 2003/09/29 00:32:40 $
+ */
+public class TransactionManagerImpl implements TransactionManager {
+    private final TransactionLog txnLog = new UnrecoverableLog();
+    private final XidFactory xidFactory = new XidFactory();
+    private volatile int timeout;
+    private ThreadLocal threadTx = new ThreadLocal();
+
+    public Transaction getTransaction() throws SystemException {
+        return (Transaction) threadTx.get();
+    }
+
+    public void setTransactionTimeout(int seconds) throws SystemException {
+        timeout = seconds;
+    }
+
+    public int getStatus() throws SystemException {
+        Transaction tx = getTransaction();
+        return (tx != null) ? tx.getStatus() : Status.STATUS_NO_TRANSACTION;
+    }
+
+    public void begin() throws NotSupportedException, SystemException {
+        if (getStatus() != Status.STATUS_NO_TRANSACTION) {
+            throw new NotSupportedException("Nested Transactions are not supported");
+        }
+        TransactionImpl tx = new TransactionImpl(xidFactory, txnLog);
+        threadTx.set(tx);
+    }
+
+    public Transaction suspend() throws SystemException {
+        Transaction tx = getTransaction();
+        threadTx.set(null);
+        return tx;
+    }
+
+    public void resume(Transaction tx) throws IllegalStateException, InvalidTransactionException, SystemException {
+        if (threadTx.get() != null) {
+            throw new IllegalStateException("Transaction already associated with current thread");
+        }
+        if (tx instanceof TransactionImpl == false) {
+            throw new InvalidTransactionException("Cannot resume foreign transaction: " + tx);
+        }
+        threadTx.set(tx);
+    }
+
+    public void setRollbackOnly() throws IllegalStateException, SystemException {
+        Transaction tx = getTransaction();
+        if (tx == null) {
+            throw new IllegalStateException("No transaction associated with current thread");
+        }
+        tx.setRollbackOnly();
+    }
+
+    public void commit() throws HeuristicMixedException, HeuristicRollbackException, IllegalStateException, RollbackException, SecurityException, SystemException {
+        Transaction tx = getTransaction();
+        if (tx == null) {
+            throw new IllegalStateException("No transaction associated with current thread");
+        }
+        try {
+            tx.commit();
+        } finally {
+            threadTx.set(null);
+        }
+    }
+
+    public void rollback() throws IllegalStateException, SecurityException, SystemException {
+        Transaction tx = getTransaction();
+        if (tx == null) {
+            throw new IllegalStateException("No transaction associated with current thread");
+        }
+        try {
+            tx.rollback();
+        } finally {
+            threadTx.set(null);
+        }
+    }
+}
---
+++ modules/core/src/test/org/apache/geronimo/transaction/manager/MockResource.java
@@ -0,0 +1,133 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ *    "Apache Geronimo" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    "Apache Geronimo", nor may "Apache" appear in their name, without
+ *    prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * ====================================================================
+ */
+package org.apache.geronimo.transaction.manager;
+
+import javax.transaction.xa.XAException;
+import javax.transaction.xa.XAResource;
+import javax.transaction.xa.Xid;
+
+/**
+ *
+ *
+ * @version $Revision: 1.1 $ $Date: 2003/09/29 00:32:40 $
+ */
+public class MockResource implements XAResource {
+    private Xid xid;
+    private MockResourceManager manager;
+    private int timeout = 0;
+
+    public MockResource(MockResourceManager manager) {
+        this.manager = manager;
+    }
+
+    public int getTransactionTimeout() throws XAException {
+        return timeout;
+    }
+
+    public boolean setTransactionTimeout(int seconds) throws XAException {
+        return false;
+    }
+
+    public Xid getXid() {
+        return xid;
+    }
+
+    public void start(Xid xid, int flags) throws XAException {
+        if (this.xid != null) {
+            throw new XAException(XAException.XAER_PROTO);
+        }
+        if ((flags & XAResource.TMJOIN) != 0) {
+            manager.join(xid, this);
+        } else {
+            manager.newTx(xid, this);
+        }
+        this.xid = xid;
+    }
+
+    public void end(Xid xid, int flags) throws XAException {
+        if (this.xid != xid) {
+            throw new XAException(XAException.XAER_INVAL);
+        }
+        this.xid = null;
+    }
+
+    public int prepare(Xid xid) throws XAException {
+        return 0;
+    }
+
+    public void commit(Xid xid, boolean onePhase) throws XAException {
+    }
+
+    public void rollback(Xid xid) throws XAException {
+        manager.forget(xid, this);
+    }
+
+    public boolean isSameRM(XAResource xaResource) throws XAException {
+        if (xaResource instanceof MockResource) {
+            return manager == ((MockResource) xaResource).manager;
+        }
+        return false;
+    }
+
+    public void forget(Xid xid) throws XAException {
+        throw new UnsupportedOperationException();
+    }
+
+    public Xid[] recover(int flag) throws XAException {
+        throw new UnsupportedOperationException();
+    }
+
+}
