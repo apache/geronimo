37085e5ec2cd2bd34181b258aa9860507230182c
Alan Cabrera <adc@apache.org>
2004-01-28 05:55:39 +0000

    New JMX remoting test
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@44963 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/remoting/src/test/org/apache/geronimo/remoting/JMXRemotingTest.java
@@ -0,0 +1,143 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ *    "Apache Geronimo" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    "Apache Geronimo", nor may "Apache" appear in their name, without
+ *    prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * ====================================================================
+ */
+package org.apache.geronimo.remoting;
+
+import javax.management.ObjectName;
+import javax.management.MBeanServer;
+import java.net.URI;
+import java.util.Collections;
+
+import junit.framework.TestCase;
+import org.apache.geronimo.gbean.jmx.GBeanMBean;
+import org.apache.geronimo.kernel.Kernel;
+import org.apache.geronimo.kernel.jmx.MBeanProxyFactory;
+import org.apache.geronimo.remoting.MarshalledObject;
+import org.apache.geronimo.remoting.jmx.RemoteMBeanServerFactory;
+import org.apache.geronimo.remoting.transport.TransportLoader;
+import org.apache.geronimo.remoting.transport.BytesMarshalledObject;
+
+
+/**
+ * @version $Revision: 1.1 $ $Date: 2004/01/28 05:55:39 $
+ */
+
+public class JMXRemotingTest extends TestCase {
+    private Kernel kernel;
+    ObjectName subsystemRouter;
+    ObjectName asyncTransport;
+    ObjectName jmxRouter;
+    ObjectName serverStub;
+    URI connectURI;
+    MBeanServer remoteProxy;
+
+    public void setUp() throws Exception {
+        kernel = new Kernel("simple.geronimo.test");
+        kernel.boot();
+
+        GBeanMBean gbean;
+
+        // Create all the parts
+        gbean = new GBeanMBean("org.apache.geronimo.remoting.router.SubsystemRouter");
+        subsystemRouter = new ObjectName("geronimo.remoting:router=SubsystemRouter");
+        kernel.loadGBean(subsystemRouter, gbean);
+
+        gbean = new GBeanMBean("org.apache.geronimo.remoting.transport.TransportLoader");
+        gbean.setAttribute("BindURI", new URI("async://0.0.0.0:0"));
+        gbean.setReferencePatterns("Router", Collections.singleton(subsystemRouter));
+        asyncTransport = new ObjectName("geronimo.remoting:transport=async");
+        kernel.loadGBean(asyncTransport, gbean);
+
+        gbean = new GBeanMBean("org.apache.geronimo.remoting.router.JMXRouter");
+        gbean.setReferencePatterns("SubsystemRouter", Collections.singleton(subsystemRouter));
+        jmxRouter = new ObjectName("geronimo.remoting:router=JMXRouter");
+        kernel.loadGBean(jmxRouter, gbean);
+
+        gbean = new GBeanMBean("org.apache.geronimo.remoting.jmx.MBeanServerStub");
+        serverStub = new ObjectName("geronimo.remoting:target=MBeanServerStub");
+        kernel.loadGBean(serverStub, gbean);
+
+        kernel.startGBean(subsystemRouter);
+        kernel.startGBean(asyncTransport);
+        kernel.startGBean(jmxRouter);
+        kernel.startGBean(serverStub);
+
+        TransportLoader bean = (TransportLoader) MBeanProxyFactory.getProxy(TransportLoader.class, kernel.getMBeanServer(), asyncTransport);
+        connectURI = bean.getClientConnectURI();
+
+        // simulate remote copy of handle
+        MarshalledObject mo = new BytesMarshalledObject(RemoteMBeanServerFactory.create(connectURI.getHost(), connectURI.getPort()));
+        remoteProxy = (MBeanServer)mo.get();
+    }
+
+    protected void tearDown() throws Exception {
+        kernel.stopGBean(serverStub);
+        kernel.stopGBean(jmxRouter);
+        kernel.stopGBean(asyncTransport);
+        kernel.stopGBean(subsystemRouter);
+
+        kernel.unloadGBean(subsystemRouter);
+        kernel.unloadGBean(asyncTransport);
+        kernel.unloadGBean(jmxRouter);
+        kernel.unloadGBean(serverStub);
+
+        kernel.shutdown();
+    }
+
+    public void testSomething() throws Exception {
+        assertEquals("Should be able to obtain default domain", "simple.geronimo.test", remoteProxy.getDefaultDomain());
+        assertTrue("There should be a couple of beans", remoteProxy.getMBeanCount().intValue() > 0);
+    }
+}
