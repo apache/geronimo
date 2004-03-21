03fb1c2e4966a6189ea4c7c7261d240f4834033e
Alan Cabrera <adc@apache.org>
2004-03-21 17:49:22 +0000

    I can use this for my networking configuration.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@45283 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/common/src/java/org/apache/geronimo/common/xml/XmlBeansUtil.java
@@ -0,0 +1,67 @@
+/**
+ *
+ * Copyright 2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.geronimo.common.xml;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.net.URL;
+
+import org.apache.xmlbeans.XmlObject;
+import org.apache.xmlbeans.SchemaType;
+import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.SchemaTypeLoader;
+import org.apache.xmlbeans.XmlBeans;
+import org.apache.xmlbeans.XmlOptions;
+
+/**
+ *
+ *
+ * @version $Revision: 1.1 $ $Date: 2004/03/21 17:49:22 $
+ *
+ * */
+public class XmlBeansUtil {
+
+    private XmlBeansUtil() {}
+
+    public static XmlObject getPlan(URL planURL, SchemaType type) {
+        InputStream is;
+        try {
+            is = planURL.openStream();
+            try {
+                return parse(is, type);
+            } finally {
+                is.close();
+            }
+        } catch (IOException e) {
+            return null;
+        } catch (XmlException e) {
+            return null;
+        }
+    }
+
+    public static XmlObject parse(InputStream is, SchemaType type) throws IOException, XmlException {
+        ArrayList errors = new ArrayList();
+        SchemaTypeLoader loader = XmlBeans.getContextTypeLoader();
+        XmlOptions options = new XmlOptions();
+        options.setLoadLineNumbers();
+        options.setErrorListener(errors);
+        return loader.parse(is, type, options);
+    }
+
+}
