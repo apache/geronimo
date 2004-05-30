b336e40c43a87d2de49d4225a4d78b5352119276
David Jencks <djencks@apache.org>
2004-05-30 18:35:06 +0000

    Property editors needed by connectors.  Hard to believe these aren't supplied.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@45447 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/common/src/java/org/apache/geronimo/common/propertyeditor/CharacterEditor.java
@@ -0,0 +1,40 @@
+/**
+ *
+ * Copyright 2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.geronimo.common.propertyeditor;
+
+/**
+ *
+ *
+ * @version $Revision: 1.1 $ $Date: 2004/05/30 18:35:06 $
+ *
+ * */
+public class CharacterEditor extends TextPropertyEditorSupport {
+    public Object getValue()
+    {
+        try {
+            String text = getAsText();
+            if (text.length() != 1) {
+                throw new IllegalArgumentException("wrong size: " + text);
+            }
+            return new Character(text.toCharArray()[0]);
+        }
+        catch (Exception e) {
+            throw new PropertyEditorException(e);
+        }
+    }
+}
