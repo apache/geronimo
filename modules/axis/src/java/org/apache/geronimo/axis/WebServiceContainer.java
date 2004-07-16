a1184905d1f023e2f24b698dfc2e7579ce5a2346
Alan Cabrera <adc@apache.org>
2004-07-16 01:09:37 +0000

    Updated pattern ResourceManagers.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@45652 13f79535-47bb-0310-9956-ffa450edef68

--- modules/axis/src/java/org/apache/geronimo/axis/WebServiceContainer.java
+++ modules/axis/src/java/org/apache/geronimo/axis/WebServiceContainer.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2001-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -112,7 +112,7 @@ public class WebServiceContainer {
         patterns.add(
                 ObjectName.getInstance(
                         "geronimo.server:j2eeType=JCAManagedConnectionFactory,*"));
-        tm.setReferencePatterns("resourceManagers", patterns);
+        tm.setReferencePatterns("ResourceManagers", patterns);
         start(tmName, tm);
         ctc = new GBeanMBean(ConnectionTrackingCoordinator.GBEAN_INFO);
         start(tcaName, ctc);
