68a0ef8991cee63a2c9d5deae5be172db086636f
Jeremy Boynes <jboynes@apache.org>
2004-07-22 00:41:23 +0000

    fix license text
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@45695 13f79535-47bb-0310-9956-ffa450edef68

--- modules/deployment/src/java/org/apache/geronimo/deployment/RemoteDeployer.java
+++ modules/deployment/src/java/org/apache/geronimo/deployment/RemoteDeployer.java
@@ -1,3 +1,20 @@
+/**
+ *
+ * Copyright 2003-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.apache.geronimo.deployment;
 
 import java.io.File;
@@ -13,11 +30,7 @@ import org.apache.geronimo.deployment.plugin.factories.DeploymentFactoryImpl;
 import org.apache.geronimo.deployment.plugin.TargetModuleIDImpl;
 
 /**
- * Created by IntelliJ IDEA.
- * User: jeremy
- * Date: Jun 23, 2004
- * Time: 1:23:53 PM
- * To change this template use File | Settings | File Templates.
+ * @version $Revision: 1.3 $ $Date: 2004/07/22 00:41:23 $
  */
 public class RemoteDeployer {
     public static void main(String[] args) throws Exception {
