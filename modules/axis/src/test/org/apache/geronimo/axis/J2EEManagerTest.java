ab5d47cb13acb0243fc3f81f856ae85a6c375ed5
Davanum Srinivas <dims@apache.org>
2004-10-08 15:16:35 +0000

    Add missing files...
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@54091 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/axis/src/test/org/apache/geronimo/axis/J2EEManagerTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.geronimo.axis;
+
+import org.apache.geronimo.kernel.Kernel;
+
+import javax.management.ObjectName;
+
+/**
+ * <p>This test case represents the code generation with the EWS module.
+ * This test case needed the $JAVA_HOME/lib.tools.jar at the classapth.</p>
+ */
+public class J2EEManagerTest extends AbstractTestCase {
+    private ObjectName name;
+    private Kernel kernel;
+
+    public J2EEManagerTest(String testName) {
+        super(testName);
+    }
+
+    public void testEcho() throws Exception {
+        J2EEManager j2eem = new J2EEManager();
+        j2eem.startJ2EEContainer(kernel);
+        j2eem.stopJ2EEContainer(kernel);
+    }
+
+    protected void setUp() throws Exception {
+        name = new ObjectName("test:name=AxisGBean");
+        kernel = new Kernel("test.kernel", "test");
+        kernel.boot();
+    }
+
+    protected void tearDown() throws Exception {
+        kernel.shutdown();
+    }
+}
