2f838a8b2b6b16a1dd94a44b8d17c7032855b21d
Alan Cabrera <adc@apache.org>
2004-10-13 02:12:45 +0000

    JavaMail GBean
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@54721 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/mail/src/java/org/apache/geronimo/mail/MailGBean.java
@@ -0,0 +1,118 @@
+/**
+ *
+ * Copyright 2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.geronimo.mail;
+
+import javax.mail.Authenticator;
+import javax.mail.Session;
+import java.util.Properties;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.apache.geronimo.gbean.GBeanInfo;
+import org.apache.geronimo.gbean.GBeanInfoFactory;
+import org.apache.geronimo.gbean.GBeanLifecycle;
+import org.apache.geronimo.gbean.WaitingException;
+
+
+/**
+ * GBean that provides access to JavaMail Sessions.
+ *
+ * @version $Rev: $ $Date: $
+ */
+public class MailGBean implements GBeanLifecycle {
+
+    private final Log log = LogFactory.getLog(MailGBean.class);
+
+    private boolean useDefault = false;
+    private Properties properties = new Properties();
+    private Authenticator authenticator;
+
+
+    public boolean getUseDefault() {
+        return useDefault;
+    }
+
+    public void setUseDefault(boolean useDefault) {
+        this.useDefault = useDefault;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    public Authenticator getAuthenticator() {
+        return authenticator;
+    }
+
+    public void setAuthenticator(Authenticator authenticator) {
+        this.authenticator = authenticator;
+    }
+
+    public Object getProxy() {
+        if (useDefault) {
+            if (authenticator == null) {
+                return Session.getDefaultInstance(properties);
+            } else {
+                return Session.getDefaultInstance(properties, authenticator);
+            }
+        } else {
+            if (authenticator == null) {
+                return Session.getInstance(properties);
+            } else {
+                return Session.getInstance(properties, authenticator);
+            }
+        }
+    }
+
+    public void doStart() throws WaitingException, Exception {
+        log.info("Started - will return "
+                 + (useDefault ? "default" : "new")
+                 + " instance "
+                 + (authenticator == null ? "without" : "with")
+                 + " authenticator");
+    }
+
+    public void doStop() throws WaitingException, Exception {
+        log.info("Stopped");
+    }
+
+    public void doFail() {
+        log.info("Failed");
+    }
+
+    public static final GBeanInfo GBEAN_INFO;
+
+    static {
+        GBeanInfoFactory infoFactory = new GBeanInfoFactory(MailGBean.class);
+
+        infoFactory.addAttribute("useDefault", Boolean.TYPE, true);
+        infoFactory.addAttribute("properties", Properties.class, true);
+        infoFactory.addReference("Authenticator", Authenticator.class);
+        infoFactory.addOperation("getProxy");
+
+        GBEAN_INFO = infoFactory.getBeanInfo();
+    }
+
+    public static GBeanInfo getGBeanInfo() {
+        return GBEAN_INFO;
+    }
+}
---
+++ modules/mail/src/test/org/apache/geronimo/mail/MailGBeanTest.java
@@ -0,0 +1,68 @@
+/**
+ *
+ * Copyright 2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.geronimo.mail;
+
+import javax.mail.Session;
+import javax.mail.Store;
+import javax.management.ObjectName;
+import java.util.Properties;
+
+import junit.framework.TestCase;
+
+import org.apache.geronimo.gbean.jmx.GBeanMBean;
+import org.apache.geronimo.kernel.Kernel;
+
+
+/**
+ * @version $Rev: $ $Date: $
+ */
+public class MailGBeanTest extends TestCase {
+
+    private Kernel kernel;
+    private ObjectName mailName;
+    private static final String KERNEL_NAME = "testKernel";
+
+    public void testProxy() throws Exception {
+        Object proxy = kernel.invoke(mailName, "getProxy");
+
+        assertNotNull(proxy);
+        assertTrue(proxy instanceof Session);
+
+//        Store store = ((Session) proxy).getStore();
+//        assertNotNull(store);
+
+        kernel.stopGBean(mailName);
+    }
+
+    protected void setUp() throws Exception {
+        kernel = new Kernel(KERNEL_NAME, "test.domain");
+        kernel.boot();
+
+        GBeanMBean cmf = new GBeanMBean(MailGBean.getGBeanInfo());
+        cmf.setAttribute("useDefault", new Boolean(true));
+        cmf.setAttribute("properties", new Properties());
+        mailName = ObjectName.getInstance("geronimo.server:J2EEServer=geronimo,J2EEType=JavaMailResource,name=default");
+        kernel.loadGBean(mailName, cmf);
+
+        kernel.startGBean(mailName);
+    }
+
+    protected void tearDown() throws Exception {
+        kernel.stopGBean(mailName);
+        kernel.shutdown();
+    }
+}
