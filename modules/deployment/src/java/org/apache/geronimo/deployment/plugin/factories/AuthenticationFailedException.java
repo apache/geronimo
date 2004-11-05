74ef604184f346ffaf550ce29124ddd38de842c8
Aaron Mulder <ammulder@apache.org>
2004-11-05 04:52:26 +0000

    Add license and header comment
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@56647 13f79535-47bb-0310-9956-ffa450edef68

--- modules/deployment/src/java/org/apache/geronimo/deployment/plugin/factories/AuthenticationFailedException.java
+++ modules/deployment/src/java/org/apache/geronimo/deployment/plugin/factories/AuthenticationFailedException.java
@@ -1,3 +1,20 @@
+/**
+ *
+ * Copyright 2003-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.apache.geronimo.deployment.plugin.factories;
 
 import javax.enterprise.deploy.spi.exceptions.DeploymentManagerCreationException;
@@ -6,7 +23,7 @@ import javax.enterprise.deploy.spi.exceptions.DeploymentManagerCreationException
  * A Geronimo-specific DeploymentManagerCreationException indicating that
  * there was a login failure.
  * 
- * @version $Revision 1.0 $
+ * @version $Rev: 53762 $ $Date: 2004-10-04 18:54:53 -0400 (Mon, 04 Oct 2004) $
  */
 public class AuthenticationFailedException extends DeploymentManagerCreationException {
     public AuthenticationFailedException(String s) {
