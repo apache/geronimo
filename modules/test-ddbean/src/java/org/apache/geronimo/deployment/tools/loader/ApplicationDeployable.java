018b81e6bac428b35578fb342a224dfe6a9dc644
Aaron Mulder <ammulder@apache.org>
2004-12-03 00:55:04 +0000

    Some files seem to have vaporized during the commit -- I must have   messed up something in the various copying and deleting
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@109611 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/test-ddbean/src/java/org/apache/geronimo/deployment/tools/loader/ApplicationDeployable.java
@@ -0,0 +1,52 @@
+/**
+ *
+ * Copyright 2003-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.geronimo.deployment.tools.loader;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import javax.enterprise.deploy.model.DDBean;
+import javax.enterprise.deploy.model.J2eeApplicationObject;
+import javax.enterprise.deploy.model.XpathListener;
+import javax.enterprise.deploy.model.exceptions.DDBeanCreateException;
+import javax.enterprise.deploy.shared.ModuleType;
+
+/**
+ * 
+ * 
+ * @version $Rev$ $Date$
+ */
+public abstract class ApplicationDeployable extends AbstractDeployable implements J2eeApplicationObject {
+    private final Map uriMap;
+    public ApplicationDeployable(URL moduleURL) throws DDBeanCreateException {
+        super(ModuleType.EAR, moduleURL, "META-INF/application.xml");
+        DDBean[] moduleBeans = getChildBean("/application/module");
+        uriMap = new HashMap(moduleBeans.length);
+        for (int i = 0; i < moduleBeans.length; i++) {
+            DDBean moduleBean = moduleBeans[i];
+            String uri;
+
+        }
+    }
+
+    public void addXpathListener(ModuleType type, String xpath, XpathListener xpl) {
+    }
+
+    public void removeXpathListener(ModuleType type, String xpath, XpathListener xpl) {
+    }
+}
