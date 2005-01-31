83e6d0229a534ad814c623d5b66b0502b90bbb1f
Jeremy Boynes <jboynes@apache.org>
2005-01-31 23:05:45 +0000

    add a null transport that just eats messages
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@149316 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/mail/src/java/org/apache/geronimo/mail/NullTransport.java
@@ -0,0 +1,37 @@
+/**
+ *
+ * Copyright 2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.geronimo.mail;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.URLName;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class NullTransport extends Transport {
+    public NullTransport(Session session, URLName urlName) {
+        super(session, urlName);
+    }
+
+    public void sendMessage(Message message, Address[] addresses) throws MessagingException {
+        // do nothing
+    }
+}
