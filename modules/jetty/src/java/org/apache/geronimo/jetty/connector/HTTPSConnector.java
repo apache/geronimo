7d013df1caa243d808bd490401659c12a827f526
Jeremy Boynes <jboynes@apache.org>
2005-04-29 18:51:02 +0000

    switch to Jetty SslConnector and start an https listener by default
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@165331 13f79535-47bb-0310-9956-ffa450edef68

--- modules/jetty/src/java/org/apache/geronimo/jetty/connector/HTTPSConnector.java
+++ modules/jetty/src/java/org/apache/geronimo/jetty/connector/HTTPSConnector.java
@@ -17,77 +17,89 @@
 
 package org.apache.geronimo.jetty.connector;
 
+import javax.net.ssl.KeyManagerFactory;
+
+import org.mortbay.http.SslListener;
+
 import org.apache.geronimo.gbean.GBeanInfo;
 import org.apache.geronimo.gbean.GBeanInfoBuilder;
+import org.apache.geronimo.j2ee.j2eeobjectnames.NameFactory;
 import org.apache.geronimo.jetty.JettyContainer;
 import org.apache.geronimo.system.serverinfo.ServerInfo;
-import org.apache.geronimo.j2ee.j2eeobjectnames.NameFactory;
-import org.mortbay.http.SunJsseListener;
 
 /**
+ * Implementation of a HTTPS connector based on Jetty's SslConnector (which uses pure JSSE).
+ *
  * @version $Rev$ $Date$
  */
 public class HTTPSConnector extends JettyConnector {
-    private final SunJsseListener https;
+    private final SslListener https;
     private final ServerInfo serverInfo;
     private String keystore;
+    private String algorithm;
 
     public HTTPSConnector(JettyContainer container, ServerInfo serverInfo) {
-        super(container, new SunJsseListener());
+        super(container, new SslListener());
         this.serverInfo = serverInfo;
-        https = (SunJsseListener)listener;
+        https = (SslListener) listener;
     }
 
     public String getKeystore() {
+        // this does not delegate to https as it needs to be resolved against ServerInfo
         return keystore;
     }
 
     public void setKeystore(String keystore) {
+        // this does not delegate to https as it needs to be resolved against ServerInfo
         this.keystore = keystore;
     }
 
-    public void setKeyPassword(String password) {
-        https.setKeyPassword(password);
+    public String getAlgorithm() {
+        return algorithm;
     }
 
-    public void setKeystoreProviderClass(String cls) {
-        https.setKeystoreProviderClass(cls);
-    }
-
-    public String getKeystoreProviderClass() {
-        return https.getKeystoreProviderClass();
-    }
-
-    public void setKeystoreProviderName(String cls) {
-        https.setKeystoreProviderName(cls);
-    }
-
-    public String getKeystoreProviderName() {
-        return https.getKeystoreProviderName();
-    }
-
-    public void setKeystoreType(String cls) {
-        https.setKeystoreType(cls);
-    }
-
-    public String getKeystoreType() {
-        return https.getKeystoreType();
+    /**
+     * Algorithm to use.
+     * As different JVMs have different implementations available, the default
+     * algorithm can be used by supplying a null value.
+     *
+     * @param algorithm the algorithm to use, or null to use the default from {@link javax.net.ssl.KeyManagerFactory#getDefaultAlgorithm()}
+     */
+    public void setAlgorithm(String algorithm) {
+        // cache the value so the null 
+        this.algorithm = algorithm;
+        if (algorithm == null) {
+            algorithm = KeyManagerFactory.getDefaultAlgorithm();
+        }
+        https.setAlgorithm(algorithm);
     }
 
     public void setPassword(String password) {
         https.setPassword(password);
     }
 
-    public void setUseDefaultTrustStore(boolean use) {
-        https.setUseDefaultTrustStore(use);
+    public void setKeyPassword(String password) {
+        https.setKeyPassword(password);
     }
 
-    public boolean getUseDefaultTrustStore() {
-        return https.getUseDefaultTrustStore();
+    public String getProtocol() {
+        return https.getProtocol();
     }
 
-    public void setNeedClientAuth(boolean auth) {
-        https.setNeedClientAuth(auth);
+    public void setProtocol(String protocol) {
+        https.setProtocol(protocol);
+    }
+
+    public String getKeystoreType() {
+        return https.getKeystoreType();
+    }
+
+    public void setKeystoreType(String keystoreType) {
+        https.setKeystoreType(keystoreType);
+    }
+
+    public void setNeedClientAuth(boolean needClientAuth) {
+        https.setNeedClientAuth(needClientAuth);
     }
 
     public boolean getNeedClientAuth() {
@@ -104,13 +116,12 @@ public class HTTPSConnector extends JettyConnector {
     static {
         GBeanInfoBuilder infoFactory = new GBeanInfoBuilder("Jetty HTTPS Connector", HTTPSConnector.class, JettyConnector.GBEAN_INFO);
         infoFactory.addAttribute("keystore", String.class, true);
+        infoFactory.addAttribute("algorithm", String.class, true);
         infoFactory.addAttribute("keyPassword", String.class, true);
-        infoFactory.addAttribute("keystoreProviderClass", String.class, true);
-        infoFactory.addAttribute("keystoreProviderName", String.class, true);
         infoFactory.addAttribute("keystoreType", String.class, true);
-        infoFactory.addAttribute("password", String.class, true);
-        infoFactory.addAttribute("useDefaultTrustStore", boolean.class, true);
         infoFactory.addAttribute("needClientAuth", boolean.class, true);
+        infoFactory.addAttribute("password", String.class, true);
+        infoFactory.addAttribute("protocol", String.class, true);
         infoFactory.addReference("ServerInfo", ServerInfo.class, NameFactory.GERONIMO_SERVICE);
         infoFactory.setConstructor(new String[]{"JettyContainer", "ServerInfo"});
         GBEAN_INFO = infoFactory.getBeanInfo();
