147c50f26da21d89bdda43bc80b6a3fa362cd2b5
Aaron Mulder <ammulder@apache.org>
2005-07-22 17:26:30 +0000

    I'm so bad about this
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@224368 13f79535-47bb-0310-9956-ffa450edef68

--- modules/kernel/src/test/org/apache/geronimo/kernel/MockParentInterface1.java
+++ modules/kernel/src/test/org/apache/geronimo/kernel/MockParentInterface1.java
@@ -1,11 +1,27 @@
+/**
+ *
+ * Copyright 2003-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.apache.geronimo.kernel;
 
 /**
- * Created by IntelliJ IDEA.
- * User: ammulder
- * Date: Jul 21, 2005
- * Time: 11:54:09 PM
- * To change this template use File | Settings | File Templates.
+ * An interface with a couple more or less arbitrary methods, used to
+ * test GBeans declaring interfaces and generating proxies based on
+ * those interfaces.
+ *
+ * @version $Rev: 220236 $ $Date: 2005-07-22 00:34:39 -0400 (Fri, 22 Jul 2005) $
  */
 public interface MockParentInterface1 {
     public String getValue();
