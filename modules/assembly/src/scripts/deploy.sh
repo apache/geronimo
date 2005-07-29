8eba4272e9e015b0ee3a58fa0d2c0922404d160f
Bruce Snyder <bsnyder@apache.org>
2005-07-29 23:17:39 +0000

    Added a deploy script.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@226421 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/assembly/src/scripts/deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#
+#  Copyright 2005 The Apache Software Foundation
+# 
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+# --------------------------------------------------------------------
+# $Rev$ $Date$
+# --------------------------------------------------------------------
+
+DEPLOYER_JAR=deployer.jar
+ARGS=
+
+if [ -z "$JAVA_HOME" ]; then
+    JAVA=`which java`
+    if [ -z "$JAVA" ]; then
+        echo "Unable to locate Java binary. Please add it to the PATH."
+        exit 1
+    fi
+    JAVA_BIN=`dirname $JAVA`
+    JAVA_HOME=$JAVA_BIN/..
+fi
+
+JAVA=$JAVA_HOME/bin/java
+
+if [ ! -f "$DEPLOYER_JAR" ]; then 
+    echo "Unable to locate the $DEPLOYER_JAR jar"
+    exit 1
+fi
+
+$JAVA $ARGS -jar $DEPLOYER_JAR "$@"
