1c0229e00e789702acfcaabc2f9033612a23c574
Aaron Mulder <ammulder@apache.org>
2005-08-03 17:57:39 +0000

    Message was overly pessimistic; can still use list-modules and start   when the RuntimeDeployer is not running
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@227238 13f79535-47bb-0310-9956-ffa450edef68

--- modules/deploy-jsr88/src/java/org/apache/geronimo/deployment/plugin/local/AbstractDeployCommand.java
+++ modules/deploy-jsr88/src/java/org/apache/geronimo/deployment/plugin/local/AbstractDeployCommand.java
@@ -62,16 +62,10 @@ public abstract class AbstractDeployCommand extends CommandSupport {
         Set deployers = kernel.listGBeans(JMXUtil.getObjectName(DEPLOYER_NAME));
         if (deployers.isEmpty()) {
             fail("No Deployer GBean present in running Geronimo server. " +
-                 "This usually indicates a very serious problem with the configuration of " +
-                 "your running Geronimo server.  Without a deployer running, you can't " +
-                 "start anything at runtime (including, of course, the deployer itself).  If " +
-                 "the deployer is present but not started, the workaround is to restart " +
-                 "the server and explicitly require the deployer on the command line.  " +
-                 "For a typical Geronimo configuration, use a startup command like: "+
-                 "'java -jar bin/server.jar org/apache/geronimo/RuntimeDeployer "+
-                 "org/apache/geronimo/SystemDatabase'.  This should fix the deployment " +
-                 "problem, but it will reset Geronimo to run no services or applications, " +
-                 "so you'll need to restart anything else that ought to be running.  " +
+                 "This usually indicates a serious problem with the configuration of " +
+                 "your running Geronimo server.  If " +
+                 "the deployer is present but not started, the workaround is to run " +
+                 "a deploy command like 'start org/apache/geronimo/RuntimeDeployer'.  " +
                  "If the deployer service is not present at all (it was undeployed) then " +
                  "you need to either re-install Geronimo or get a deployment plan for the " +
                  "runtime deployer and distribute it while the server is not running and " +
