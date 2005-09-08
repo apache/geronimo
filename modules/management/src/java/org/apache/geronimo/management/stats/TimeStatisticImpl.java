42982c603fa206e26c2304a4189186be61c5e840
Aaron Mulder <ammulder@apache.org>
2005-09-08 21:37:59 +0000

    Add another statistics type
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@279645 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/management/src/java/org/apache/geronimo/management/stats/TimeStatisticImpl.java
@@ -0,0 +1,65 @@
+/**
+ *
+ * Copyright 2003-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.geronimo.management.stats;
+
+import javax.management.j2ee.statistics.TimeStatistic;
+
+/**
+ * @version $Rev: 46019 $ $Date: 2004-09-14 05:56:06 -0400 (Tue, 14 Sep 2004) $
+ */
+public class TimeStatisticImpl extends StatisticImpl implements TimeStatistic {
+    long count;
+    long minTime;
+    long maxTime;
+    long totalTime;
+
+    public TimeStatisticImpl(String name, String unit, String description) {
+        super(name, unit, description);
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+
+    public long getMinTime() {
+        return minTime;
+    }
+
+    public void setMinTime(long minTime) {
+        this.minTime = minTime;
+    }
+
+    public long getMaxTime() {
+        return maxTime;
+    }
+
+    public void setMaxTime(long maxTime) {
+        this.maxTime = maxTime;
+    }
+
+    public long getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(long totalTime) {
+        this.totalTime = totalTime;
+    }
+}
