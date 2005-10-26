f06fca60654a82dec15ef936c246e93cb482186b
Matt Richard Hogstrom <hogstrom@apache.org>
2005-10-26 17:37:34 +0000

    Added comments for alternate database syntax factories in the deployment plan
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@328691 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/daytrader/derby/createDB.sh
+++ sandbox/daytrader/derby/createDB.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-export G_PATH=~/geronimo/geronimo/modules/assembly/target/geronimo-1.0-SNAPSHOT/repository/org.apache.derby/jars
+if [ "${GERONIMO_HOME}" = "" ]
+then
+  echo Please define the GERONIMO_HOME environment variable.
+  exit
+fi
+export G_PATH=${GERONIMO_HOME}/repository/org.apache.derby/jars
 export CLASSPATH=${G_PATH}/derby-10.1.1.0.jar
 export CLASSPATH=${CLASSPATH}:${G_PATH}/derbynet-10.1.1.0.jar
 export CLASSPATH=${CLASSPATH}:${G_PATH}/derbytools-10.1.1.0.jar
 export CLASSPATH=${CLASSPATH}:${G_PATH}/derbyclient-10.1.1.0.jar
-export CLASSPATH=${CLASSPATH}:/home/db2inst1/sqllib/java/db2jcc.jar
-export
 
-java -Dij.protocol=jdbc:derby:net://localhost:1527/ org.apache.derby.tools.ij < derby.txt
-#java -Dij.driver=com.ibm.db2.jcc.DB2Driver -Dij.protocol=jdbc:derby:net://localhost:1527/ org.apache.derby.tools.ij 
+echo "Invoking IJ command line tool to create the database and tables...please wait"
+java -Dij.driver=org.apache.derby.jdbc.ClientDriver -Dij.protocol=jdbc:derby://localhost:1527/ org.apache.derby.tools.ij < derby.txt
+echo "Table creation complete"
