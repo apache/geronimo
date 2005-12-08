a1e6eedcaafb736243872d025d886fd259c72152
Matt Richard Hogstrom <hogstrom@apache.org>
2005-12-08 09:09:21 +0000

    Removing DayTrader from Sandbox
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@355048 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/daytrader/bin/depdb2resources.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export GERONIMO_HOME=/home/hogstrom/geronimo/geronimo/modules/assembly/target/geronimo-1.0-SNAPSHOT
-
-java -jar ${GERONIMO_HOME}/bin/deployer.jar --user system --password manager deploy dayTrader.ear dayTrader-plan-db2.xml
