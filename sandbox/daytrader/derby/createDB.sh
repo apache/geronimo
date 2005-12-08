af5dfb0ecb9f3af0e05d4fc22ae8dcc204381587
Matt Richard Hogstrom <hogstrom@apache.org>
2005-12-08 07:16:14 +0000

    Day trader Updates
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@355022 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/daytrader/derby/createDB.sh
+++ sandbox/daytrader/derby/createDB.sh
@@ -9,7 +9,7 @@ export CLASSPATH=${G_PATH}/derby-10.1.1.0.jar
 export CLASSPATH=${CLASSPATH}:${G_PATH}/derbynet-10.1.1.0.jar
 export CLASSPATH=${CLASSPATH}:${G_PATH}/derbytools-10.1.1.0.jar
 export CLASSPATH=${CLASSPATH}:${G_PATH}/derbyclient-10.1.1.0.jar
-
+export
 echo "Invoking IJ command line tool to create the database and tables...please wait"
 java -Dij.driver=org.apache.derby.jdbc.ClientDriver -Dij.protocol=jdbc:derby://localhost:1527/ org.apache.derby.tools.ij < derby.txt
 echo "Table creation complete"
