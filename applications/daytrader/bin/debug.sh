61a74345c857d9a1e9fc2528eb301a5b03f24598
Matt Richard Hogstrom <hogstrom@apache.org>
2006-01-14 07:04:08 +0000

    Remove DayTrader from the 1.0.1 Branch
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/branches/1.0@368993 13f79535-47bb-0310-9956-ffa450edef68

--- applications/daytrader/bin/debug.sh
+++
@@ -1,8 +0,0 @@
-export GERONIMO_HOME=~/dev/geronimo/assemblies/j2ee-jetty-server/target/geronimo-1.0
-rm  ${GERONIMO_HOME}/var/log/geronimo.log
-
-java -Xdebug -Xnoagent -Xmx512m -Xms512m  -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar ${GERONIMO_HOME}/bin/server.jar $1
-
-#java  -Xmx1024m -Xms1024m  -jar ${GERONIMO_HOME}/bin/server.jar org/apache/geronimo/DebugConsole
-
-# java  -Xmx1024m -Xms1024m  -jar ${GERONIMO_HOME}/bin/server.jar $1
--- applications/daytrader/bin/undepresources.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#export GERONIMO_HOME=~/dev/geronimo/assemblies/j2ee-tomcat-server/geronimo-1.0
-
-java -jar ${GERONIMO_HOME}/bin/deployer.jar --user system --password manager stop Trade
-java -jar ${GERONIMO_HOME}/bin/deployer.jar --user system --password manager undeploy Trade
