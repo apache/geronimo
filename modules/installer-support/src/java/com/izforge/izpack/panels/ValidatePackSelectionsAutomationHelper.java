076dc387f0608bf4fd698fefc716e11c2bbf1e39
David Jencks <djencks@apache.org>
2006-01-21 02:32:10 +0000

    GERONIMO-1505, GERONIMO-1514, GERONIMO-1517 Fix copyright statements of modified izpack files, fix tomcat activation, install derby with j2ee-features.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/branches/1.0@370961 13f79535-47bb-0310-9956-ffa450edef68

--- modules/installer-support/src/java/com/izforge/izpack/panels/ValidatePackSelectionsAutomationHelper.java
+++ modules/installer-support/src/java/com/izforge/izpack/panels/ValidatePackSelectionsAutomationHelper.java
@@ -1,25 +1,25 @@
+/**
+ *
+ * Copyright 2003-2006 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 /*
- * modified for Geronimo....  Based on....
- * IzPack - Copyright 2001-2005 Julien Ponge, All Rights Reserved.
- * 
+ * This module was adapted from IzPack work done by Julien Ponge
+ * and Elmar Grom.
  * http://www.izforge.com/izpack/
  * http://developer.berlios.de/projects/izpack/
- * 
- * Copyright 2003 Jonathan Halliday
- * Copyright 2002 Elmar Grom
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- *     
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ */ 
 
 package com.izforge.izpack.panels;
 
