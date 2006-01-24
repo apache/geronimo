3331f8e3e3339f8fdaa6b83be70b4eba6858c0c6
David Jencks <djencks@apache.org>
2006-01-24 09:40:34 +0000

    GERONIMO-1427 Port lightweight geronimo work to trunk
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@371874 13f79535-47bb-0310-9956-ffa450edef68

--- modules/j2ee-builder/src/java/org/apache/geronimo/j2ee/deployment/UnavailableModuleBuilder.java
+++ modules/j2ee-builder/src/java/org/apache/geronimo/j2ee/deployment/UnavailableModuleBuilder.java
@@ -1,6 +1,9 @@
 package org.apache.geronimo.j2ee.deployment;
 
 import org.apache.geronimo.common.DeploymentException;
+import org.apache.geronimo.gbean.GBeanInfo;
+import org.apache.geronimo.gbean.GBeanInfoBuilder;
+import org.apache.geronimo.j2ee.j2eeobjectnames.NameFactory;
 
 import java.io.File;
 import java.util.jar.JarFile;
@@ -8,14 +11,16 @@ import java.net.URL;
 import java.net.URI;
 
 /**
+ * @version $Rev: 356097 $ $Date: 2005-12-11 17:29:03 -0800 (Sun, 11 Dec 2005) $
  */
 public class UnavailableModuleBuilder implements ModuleBuilder {
+
     public Module createModule(File plan, JarFile moduleFile) throws DeploymentException {
-        throw new DeploymentException("Module type unavailable");
+    		return null; 
     }
 
     public Module createModule(Object plan, JarFile moduleFile, String targetPath, URL specDDUrl, URI earConfigId, Object moduleContextInfo) throws DeploymentException {
-        throw new DeploymentException("Module type unavailable");
+    		return null;
     }
 
     public void installModule(JarFile earFile, EARContext earContext, Module module) throws DeploymentException {
@@ -32,5 +37,17 @@ public class UnavailableModuleBuilder implements ModuleBuilder {
 
     public String getSchemaNamespace() {
         return null;
+    }
+    
+    public static final GBeanInfo GBEAN_INFO;
+
+    static {
+        GBeanInfoBuilder infoBuilder = GBeanInfoBuilder.createStatic(UnavailableModuleBuilder.class, NameFactory.MODULE_BUILDER);
+        infoBuilder.addInterface(ModuleBuilder.class);
+	    GBEAN_INFO = infoBuilder.getBeanInfo();
+    }
+    
+    public static GBeanInfo getGBeanInfo() {
+        return GBEAN_INFO;
     }
 }
