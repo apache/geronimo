9ec177cf07fe0b1c42f3d34b9e57ed9da7235a50
Aaron Mulder <ammulder@apache.org>
2006-05-13 04:06:15 +0000

    Add some naming JavaBeans to support more DConfigBeans
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/branches/1.1@406010 13f79535-47bb-0310-9956-ffa450edef68

--- modules/naming-builder/src/java/org/apache/geronimo/naming/deployment/jsr88/GBeanLocator.java
+++ modules/naming-builder/src/java/org/apache/geronimo/naming/deployment/jsr88/GBeanLocator.java
@@ -1,16 +1,18 @@
 package org.apache.geronimo.naming.deployment.jsr88;
 
-import org.apache.geronimo.deployment.plugin.XmlBeanSupport;
 import org.apache.geronimo.xbeans.geronimo.naming.GerGbeanLocatorType;
-import org.apache.geronimo.xbeans.geronimo.naming.GerPatternType;
 
 /**
  * Represents an element of the gbean-locatorType in a Geronimo
  * deployment plan.
+ *                                     <p>
+ * Has 2 JavaBean Properties           <br />
+ *  - GBeanLink (type String)          <br />
+ *  - pattern (type Pattern)           </p>
  *
  * @version $Rev: 46019 $ $Date: 2004-09-14 05:56:06 -0400 (Tue, 14 Sep 2004) $
  */
-public class GBeanLocator extends XmlBeanSupport {
+public class GBeanLocator extends HasPattern {
     public GBeanLocator() {
         super(null);
     }
@@ -23,11 +25,6 @@ public class GBeanLocator extends XmlBeanSupport {
         return (GerGbeanLocatorType) getXmlObject();
     }
 
-    // Must be public but should not be a JavaBean property -- sigh
-    public boolean configured() {
-        return getXmlObject() != null;
-    }
-
     public void configure(GerGbeanLocatorType xml) {
         setXmlObject(xml);
     }
@@ -38,12 +35,8 @@ public class GBeanLocator extends XmlBeanSupport {
 
     public void setGBeanLink(String link) {
         GerGbeanLocatorType locator = getGBeanLocator();
-        if(link != null) {
-            if(locator.isSetPattern()) {
-                Pattern oldPattern = buildPattern(locator.getPattern());
-                locator.unsetPattern();
-                pcs.firePropertyChange("objectNameComponents", oldPattern, null);
-            }
+        if(link != null && locator.isSetPattern()) {
+            clearPatternFromChoice();
         }
         String old = getGBeanLink();
         locator.setGbeanLink(link);
@@ -51,49 +44,12 @@ public class GBeanLocator extends XmlBeanSupport {
     }
 
 
-    public Pattern buildPattern(GerPatternType patternType) {
-        Pattern group = new Pattern();
-        group.setGroupId(patternType.getGroupId());
-        group.setArtifactId(patternType.getArtifactId());
-        group.setVersion(patternType.getVersion());
-        group.setModule(patternType.getModule());
-        group.setName(patternType.getName());
-        return group.empty() ? null : group;
-    }
-
-    public void setPattern(Pattern group) {
+    protected void clearNonPatternFromChoice() {
         GerGbeanLocatorType locator = getGBeanLocator();
-        if(group != null && !group.empty()) {
-            if(locator.isSetGbeanLink()) {
-                String temp = locator.getGbeanLink();
-                locator.unsetGbeanLink();
-                pcs.firePropertyChange("GBeanLink", temp, null);
-            }
+        if(locator.isSetGbeanLink()) {
+            String temp = locator.getGbeanLink();
+            locator.unsetGbeanLink();
+            pcs.firePropertyChange("GBeanLink", temp, null);
         }
-        Pattern old = buildPattern(locator.getPattern());
-            locator.unsetPattern();
-        if(group != null) {
-            GerPatternType patternType = locator.addNewPattern();
-            if(!isEmpty(group.getGroupId())) {
-                patternType.setGroupId(group.getGroupId());
-            }
-            if(!isEmpty(group.getArtifactId())) {
-                patternType.setArtifactId(group.getArtifactId());
-            }
-            if(!isEmpty(group.getModule())) {
-                patternType.setModule(group.getModule());
-            }
-            if(!isEmpty(group.getName())) {
-                patternType.setName(group.getName());
-            }
-            if(!isEmpty(group.getVersion())) {
-                patternType.setVersion(group.getVersion());
-            }
-        }
-        pcs.firePropertyChange("objectNameComponents", old, group);
-    }
-
-    private static boolean isEmpty(String s) {
-        return s == null || s.trim().equals("");
     }
 }
