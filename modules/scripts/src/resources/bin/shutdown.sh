70c8c9d047a16a46043420658b0bcfe3db9cde8c
John Robert Sisson <jsisson@apache.org>
2006-05-21 01:30:06 +0000

    GERONIMO-2020 Relative symlinks to the shell scripts were not handled correctly
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/branches/1.1@408119 13f79535-47bb-0310-9956-ffa450edef68

--- modules/scripts/src/resources/bin/shutdown.sh
+++ modules/scripts/src/resources/bin/shutdown.sh
@@ -51,7 +51,7 @@ PRG="$0"
 while [ -h "$PRG" ] ; do
   ls=`ls -ld "$PRG"`
   link=`expr "$ls" : '.*-> \(.*\)$'`
-  if expr "$link" : '.*/.*' > /dev/null; then
+  if expr "$link" : '/.*' > /dev/null; then
     PRG="$link"
   else
     PRG=`dirname "$PRG"`/"$link"
