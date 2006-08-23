55f0defedcfabb34dbd995313a1627867563418c
Jason Dillon <jdillon@apache.org>
2006-08-23 01:54:39 +0000

    Changed the bootstrapping to be handled by Ant for cross platform compatibility
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/trunk@433827 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bootstrap.bat
@@ -0,0 +1,21 @@
+@echo off
+rem 
+rem $Rev$ $Date$
+rem 
+
+if not "%ECHO%" == "" echo %ECHO%
+
+setlocal
+set DIRNAME=%~dp0%
+set PROGNAME=%~nx0%
+set ARGS=%*
+
+ant %BUILD_OPTIONS% -f bootstrap.xml %ARGS%
+
+:END
+
+endlocal
+
+if not "%PAUSE%" == "" pause
+
+:END_NO_PAUSE
