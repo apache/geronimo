a02d253e2612dcdcbf87b8ce97c068093ea22335
Jason Dillon <jdillon@apache.org>
2006-09-04 00:28:23 +0000

    Moved assembly config and helpers to InstallerMojoSupport
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@439884 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugins/geronimo-maven-plugin/src/main/java/org/apache/geronimo/mavenplugins/geronimo/ServerMojoSupport.java
+++ maven-plugins/geronimo-maven-plugin/src/main/java/org/apache/geronimo/mavenplugins/geronimo/ServerMojoSupport.java
@@ -16,22 +16,11 @@
 
 package org.apache.geronimo.mavenplugins.geronimo;
 
-import org.apache.maven.project.MavenProject;
-import org.apache.maven.artifact.resolver.ArtifactResolver;
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-
-import java.util.Map;
-import java.util.HashMap;
-import java.io.File;
-
 import org.apache.geronimo.genesis.AntMojoSupport;
+import org.apache.maven.project.MavenProject;
 
 /**
- * Support for Geronimo {start, stop} server mojos.
+ * Support for Geronimo server mojos.
  *
  * @version $Rev$ $Date$
  */
@@ -60,49 +49,6 @@ public abstract class ServerMojoSupport
     protected String password = null;
 
     //
-    // TODO: Move assembly bits to InstallerMojoSupport
-    //
-    
-    /**
-     * List of assembly artifact configurations.  Artifacts need to point to ZIP archives.
-     *
-     * @parameter
-     * @required
-     */
-    protected AssemblyConfig[] assemblies = null;
-
-    /**
-     * Identifer of the assembly configuration to use.
-     *
-     * @parameter expression="${assemblyId}"
-     */
-    protected String assemblyId = null;
-
-    /**
-     * The default assemblyId to use when no assemblyId configured.
-     *
-     * @parameter
-     */
-    protected String defaultAssemblyId = null;
-
-    /**
-     * A file which points to a specific assembly ZIP archive.
-     * If this parameter is set, then it will be used instead of from the
-     * assemblies configuration.
-     *
-     * @parameter expression="${assemblyArchive}"
-     */
-    protected File assemblyArchive = null;
-
-    /**
-     * Directory to extract the assembly into.
-     *
-     * @parameter expression="${project.build.directory}"
-     * @required
-     */
-    protected File outputDirectory = null;
-
-    //
     // MojoSupport Hooks
     //
 
@@ -117,135 +63,5 @@ public abstract class ServerMojoSupport
 
     protected MavenProject getProject() {
         return project;
-    }
-
-    /**
-     * ???
-     *
-     * @component
-     * @required
-     * @readonly
-     */
-    protected ArtifactFactory artifactFactory = null;
-
-    protected ArtifactFactory getArtifactFactory() {
-        return artifactFactory;
-    }
-
-    /**
-     * ???
-     *
-     * @component
-     * @required
-     * @readonly
-     */
-    protected ArtifactResolver artifactResolver = null;
-
-    protected ArtifactResolver getArtifactResolver() {
-        return artifactResolver;
-    }
-
-    /**
-     * ???
-     *
-     * @parameter expression="${localRepository}"
-     * @readonly
-     * @required
-     */
-    protected ArtifactRepository artifactRepository = null;
-
-    protected ArtifactRepository getArtifactRepository() {
-        return artifactRepository;
-    }
-
-    /**
-     * The assembly archive to use when installing.
-     */
-    protected File installArchive;
-
-    /**
-     * The directory where the assembly has been installed to.
-     */
-    protected File installDir;
-
-    protected void init() throws MojoExecutionException, MojoFailureException {
-        super.init();
-
-        // Determine which archive and directory to use... either manual or from artifacts
-        if (assemblyArchive != null) {
-            log.debug("Using non-artifact based assembly archive: " + installArchive);
-
-            installArchive = assemblyArchive;
-
-            //
-            // FIXME: This probably will not work...
-            //
-
-            installDir = new File(outputDirectory, "assembly-archive");
-        }
-        else {
-            Artifact artifact = getAssemblyArtifact();
-
-            if (!"zip".equals(artifact.getType())) {
-                throw new MojoExecutionException("Assembly file does not look like a ZIP archive");
-            }
-
-            installArchive = artifact.getFile();
-            installDir = new File(outputDirectory, artifact.getArtifactId() + "-" + artifact.getVersion());
-        }
-    }
-
-    protected Artifact getAssemblyArtifact() throws MojoExecutionException {
-        assert assemblies != null;
-
-        AssemblyConfig config;
-
-        if (assemblies.length == 0) {
-            throw new MojoExecutionException("At least one assembly configuration must be specified");
-        }
-        else if (assemblies.length > 1 && assemblyId == null && defaultAssemblyId == null) {
-            throw new MojoExecutionException("Must specify assemblyId (or defaultAssemblyId) when more than on assembly configuration is given");
-        }
-        else if (assemblies.length == 1) {
-            config = assemblies[0];
-        }
-        else {
-            if (assemblyId == null) {
-                assemblyId = defaultAssemblyId;
-            }
-
-            log.debug("Searching for assembly config for id: " + assemblyId);
-
-            // Make sure there are no duplicate ids
-            Map idMap = new HashMap();
-
-            for (int i=0; i < assemblies.length; i++) {
-                String id = assemblies[i].getId();
-
-                if (id == null) {
-                    throw new MojoExecutionException("Missing id for assembly configuration: " + assemblies[i]);
-                }
-
-                if (idMap.containsKey(id)) {
-                    throw new MojoExecutionException("Duplicate assembly id: " + id);
-                }
-
-                idMap.put(id, assemblies[i]);
-            }
-
-            config = (AssemblyConfig) idMap.get(assemblyId);
-            if (config == null) {
-                throw new MojoExecutionException("Missing assembly configuration for id: " + assemblyId);
-            }
-        }
-
-        log.info("Using assembly configuration: " + config);
-        Artifact artifact = getArtifact(config);
-
-        if (artifact.getFile() == null) {
-            throw new MojoExecutionException("Assembly artifact does not have an attached file: " + artifact);
-        }
-
-        return artifact;
     }
 }
