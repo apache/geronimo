5b7f091a8563f511a9532d8ee7507eb76789cba8
Jason Dillon <jdillon@apache.org>
2006-09-13 07:20:26 +0000

    A place to test deployments
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@442879 13f79535-47bb-0310-9956-ffa450edef68

---
+++ testsuite/deployment-testsuite/basic/src/test/java/org/apache/geronimo/testsuite/console/LinkCheckTest.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.geronimo.testsuite.console;
+
+/**
+ * ???
+ *
+ * @version $Rev$ $Date$
+ */
+public class LinkCheckTest
+    extends ConsoleTestSupport
+{
+    /**
+     * @testng.test
+     */
+    public void testServerInfoLink() throws Exception {
+        login();
+        
+        selenium.click("link=Information");
+        selenium.waitForPageToLoad("30000");
+        assertEquals("Geronimo Console", selenium.getTitle());
+        
+        assertTrue(selenium.isTextPresent("Geronimo Version"));
+        assertTrue(selenium.isTextPresent("Geronimo Start Time"));
+        
+        assertEquals("Server Info", selenium.getText(
+            "xpath=/html/body/table[@id='rootfragment']/tbody/tr[2]/td/table/tbody/tr[2]/td[4]/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/table/tbody/tr/td[2]/table/tbody/tr/td[1]/strong"));
+        
+        logout();
+    }
+}
+
