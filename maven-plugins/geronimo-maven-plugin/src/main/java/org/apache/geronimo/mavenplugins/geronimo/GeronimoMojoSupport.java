69a74086c5930e18cc9d1a015cd6b38015b13430
Jason Dillon <jdillon@apache.org>
2006-10-09 20:42:07 +0000

    Remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@454498 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugins/geronimo-maven-plugin/src/main/java/org/apache/geronimo/mavenplugins/geronimo/GeronimoMojoSupport.java
+++ maven-plugins/geronimo-maven-plugin/src/main/java/org/apache/geronimo/mavenplugins/geronimo/GeronimoMojoSupport.java
@@ -20,8 +20,6 @@
 package org.apache.geronimo.mavenplugins.geronimo;
 
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 
 import org.apache.geronimo.genesis.ant.AntMojoSupport;
