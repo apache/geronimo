f705428c38d0298c89d3435bae6fcf28f6c22ae5
Prasad Kashyap <prasad@apache.org>
2006-12-01 05:08:07 +0000

    * changed to use RemoteTestServer
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@481157 13f79535-47bb-0310-9956-ffa450edef68

--- testsuite/ejbcontainer-testsuite/test-ejbcontainer/src/test/java/org/apache/openejb/test/ReuseOpenEJBTest.java
+++ testsuite/ejbcontainer-testsuite/test-ejbcontainer/src/test/java/org/apache/openejb/test/ReuseOpenEJBTest.java
@@ -31,7 +31,7 @@ public class ReuseOpenEJBTest extends TestCase {
 
     public static Test suite() {
         System.setProperty("openejb.assembler", org.apache.openejb.assembler.Assembler.class.getName());
-        System.setProperty("openejb.test.server", org.apache.openejb.test.IvmTestServer.class.getName());
+        System.setProperty("openejb.test.server", org.apache.openejb.test.RemoteTestServer.class.getName());
         System.setProperty("openejb.test.database", org.apache.openejb.test.InstantDbTestDatabase.class.getName());
         return ClientTestSuite.suite();
     }
