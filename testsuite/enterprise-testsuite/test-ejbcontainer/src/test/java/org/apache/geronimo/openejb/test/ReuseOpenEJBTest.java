ae0ac42e3050d73788dba9cf1987a7729c55fb1c
Prasad Kashyap <prasad@apache.org>
2006-12-12 18:58:21 +0000

    inserted geronimo in the package name
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@486277 13f79535-47bb-0310-9956-ffa450edef68

--- testsuite/enterprise-testsuite/test-ejbcontainer/src/test/java/org/apache/geronimo/openejb/test/ReuseOpenEJBTest.java
+++ testsuite/enterprise-testsuite/test-ejbcontainer/src/test/java/org/apache/geronimo/openejb/test/ReuseOpenEJBTest.java
@@ -15,7 +15,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.apache.openejb.test;
+package org.apache.geronimo.openejb.test;
 
 import junit.framework.TestCase;
 import junit.framework.Test;
