69965813cd12dee3098a628bd7360f5aaaeb7f3e
Davanum Srinivas <dims@apache.org>
2007-01-26 21:20:29 +0000

    Fix for GERONIMO-2781 - Improved CXF-based POJO WebService support
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@500353 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/geronimo-cxf/src/main/java/org/apache/geronimo/cxf/CXFWebServiceContext.java
@@ -0,0 +1,177 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.geronimo.cxf;
+
+import java.security.Principal;
+
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.cxf.jaxws.context.WebServiceContextImpl;
+
+/**
+ * Implementation of WebServiceContext for POJO WS to ensure that getUserPrincipal()
+ * and isUserInRole() are properly handled.
+ */
+public class CXFWebServiceContext implements WebServiceContext {
+
+    private WebServiceContextImpl ctx = new WebServiceContextImpl();
+
+    public final MessageContext getMessageContext() {
+        // TODO: if null consider throwing IllegalStateException?
+        return ctx.getMessageContext();
+    }
+    
+    private HttpServletRequest getHttpServletRequest() {
+        MessageContext ctx = getMessageContext();
+        return (ctx != null) ? (HttpServletRequest)ctx.get(MessageContext.SERVLET_REQUEST) : null;
+    }
+    
+    public final Principal getUserPrincipal() {
+        HttpServletRequest request = getHttpServletRequest();
+        return (request != null) ? request.getUserPrincipal() : null;
+    }
+    
+    public final boolean isUserInRole(String user) {
+        HttpServletRequest request = getHttpServletRequest();
+        return (request != null) ? request.isUserInRole(user) : false;
+    }
+    
+}
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.geronimo.cxf;
+
+import java.security.Principal;
+
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.cxf.jaxws.context.WebServiceContextImpl;
+
+/**
+ * Implementation of WebServiceContext for POJO WS to ensure that getUserPrincipal()
+ * and isUserInRole() are properly handled.
+ */
+public class CXFWebServiceContext implements WebServiceContext {
+
+    private WebServiceContextImpl ctx = new WebServiceContextImpl();
+
+    public final MessageContext getMessageContext() {
+        // TODO: if null consider throwing IllegalStateException?
+        return ctx.getMessageContext();
+    }
+    
+    private HttpServletRequest getHttpServletRequest() {
+        MessageContext ctx = getMessageContext();
+        return (ctx != null) ? (HttpServletRequest)ctx.get(MessageContext.SERVLET_REQUEST) : null;
+    }
+    
+    public final Principal getUserPrincipal() {
+        HttpServletRequest request = getHttpServletRequest();
+        return (request != null) ? request.getUserPrincipal() : null;
+    }
+    
+    public final boolean isUserInRole(String user) {
+        HttpServletRequest request = getHttpServletRequest();
+        return (request != null) ? request.isUserInRole(user) : false;
+    }
+    
+}
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.geronimo.cxf;
+
+import java.security.Principal;
+
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.cxf.jaxws.context.WebServiceContextImpl;
+
+/**
+ * Implementation of WebServiceContext for POJO WS to ensure that getUserPrincipal()
+ * and isUserInRole() are properly handled.
+ */
+public class CXFWebServiceContext implements WebServiceContext {
+
+    private WebServiceContextImpl ctx = new WebServiceContextImpl();
+
+    public final MessageContext getMessageContext() {
+        // TODO: if null consider throwing IllegalStateException?
+        return ctx.getMessageContext();
+    }
+    
+    private HttpServletRequest getHttpServletRequest() {
+        MessageContext ctx = getMessageContext();
+        return (ctx != null) ? (HttpServletRequest)ctx.get(MessageContext.SERVLET_REQUEST) : null;
+    }
+    
+    public final Principal getUserPrincipal() {
+        HttpServletRequest request = getHttpServletRequest();
+        return (request != null) ? request.getUserPrincipal() : null;
+    }
+    
+    public final boolean isUserInRole(String user) {
+        HttpServletRequest request = getHttpServletRequest();
+        return (request != null) ? request.isUserInRole(user) : false;
+    }
+    
+}
+
