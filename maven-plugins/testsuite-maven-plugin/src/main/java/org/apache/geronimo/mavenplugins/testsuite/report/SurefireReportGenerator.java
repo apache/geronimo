80cd135e72de6d5fc6fc57da2f774804e550c28d
Vamsavardhana Chillakuru <vamsic007@apache.org>
2007-02-08 05:25:31 +0000

    Blank space in license header causing parsing/compilation grief.  o Was fixed in trunk in rev 476719
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/branches/1.2@504789 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugins/testsuite-maven-plugin/src/main/java/org/apache/geronimo/mavenplugins/testsuite/report/SurefireReportGenerator.java
+++ maven-plugins/testsuite-maven-plugin/src/main/java/org/apache/geronimo/mavenplugins/testsuite/report/SurefireReportGenerator.java
@@ -5,15 +5,15 @@
  *  The ASF licenses this file to You under the Apache License, Version 2.0
  *  (the "License"); you may not use this file except in compliance with
  *  the License.  You may obtain a copy of the License at
-  *
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *  
  * Unless required by applicable law or agreed to in writing, software
- * di stributed under the License is distributed on an "AS IS" BASIS,
- * WI THOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * Se e the License for the specific language governing permissions and
- * li mitations under the License.
- * /
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
  
 package org.apache.geronimo.mavenplugins.testsuite.report;
 
@@ -30,6 +30,9 @@ import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.StringTokenizer;
 
+/**
+ * @version $Rev$ $Date$
+ */
 public class SurefireReportGenerator
 {
     private SurefireReportParser report;
