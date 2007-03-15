3f84b006dc570aa0064f01914f2e3b2dc92c00c9
Paul Franklin McMahan <pmcmahan@apache.org>
2007-03-15 20:28:59 +0000

    check in src file missing from 501716
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@518746 13f79535-47bb-0310-9956-ffa450edef68

---
+++ testsuite/web-testsuite/test-2.1-jsps/src/main/java/org/apache/geronimo/testsuite/jsps/HelloTag.java
@@ -0,0 +1,37 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.geronimo.testsuite.jsps;
+
+import java.io.IOException;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class HelloTag extends TagSupport {
+   public int doStartTag() throws JspException {
+      try {
+          pageContext.getOut().print("Hello");
+      } catch (Exception e) {
+          throw new JspTagException(e.getMessage());
+      }
+      return SKIP_BODY;
+   }
+   public int doEndTag() {
+      return EVAL_PAGE;
+   }
+}
