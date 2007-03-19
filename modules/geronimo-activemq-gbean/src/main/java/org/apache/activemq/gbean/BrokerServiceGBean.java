6866908ad7a439ce7516128bb368a4868fee22e4
Jason Dillon <jdillon@apache.org>
2007-03-19 01:01:01 +0000

    r6663@Bliss:  jason | 2007-03-18 15:28:48 -0700  Branch for server/trunk re-org  r6664@Bliss:  jason | 2007-03-18 15:38:19 -0700  New parent dir  r6665@Bliss:  jason | 2007-03-18 15:41:22 -0700  new dir  r6666@Bliss:  jason | 2007-03-18 15:48:27 -0700  Repackage amq stuff into org.apache.geronimo.activemq  r6667@Bliss:  jason | 2007-03-18 15:57:00 -0700  Rename geronimo-activemq-gbean* modules to geronimo-activemq*  r6668@Bliss:  jason | 2007-03-18 16:00:24 -0700  Fixing up packages  r6669@Bliss:  jason | 2007-03-18 16:03:31 -0700  Drop gbean package  r6670@Bliss:  jason | 2007-03-18 16:52:18 -0700  Update references to new activemq module names
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@519774 13f79535-47bb-0310-9956-ffa450edef68

--- modules/geronimo-activemq-gbean/src/main/java/org/apache/activemq/gbean/BrokerServiceGBean.java
+++ modules/geronimo-activemq-gbean/src/main/java/org/apache/activemq/gbean/BrokerServiceGBean.java
@@ -28,4 +28,37 @@ import org.apache.activemq.gbean.ActiveMQBroker;
 public interface BrokerServiceGBean extends ActiveMQBroker {
     BrokerService getBrokerContainer();
     String getBrokerName();
-}
+}==== ORIGINAL VERSION modules/geronimo-activemq-gbean/src/main/java/org/apache/activemq/gbean/BrokerServiceGBean.java 117426603840632
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.activemq.gbean;
+
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.gbean.ActiveMQBroker;
+
+/**
+ * An interface to the ActiveMQContainerGBean for use by the 
+ * ActiveMQConnectorGBean.
+ *
+ * @version $Revision$
+ */
+public interface BrokerServiceGBean extends ActiveMQBroker {
+	
+	abstract BrokerService getBrokerContainer();	
+	String getBrokerName();
+	
+}==== THEIR VERSION modules/geronimo-activemq-gbean/src/main/java/org/apache/activemq/gbean/BrokerServiceGBean.java (/geronimo/trunk-reorg) 117426603840632
