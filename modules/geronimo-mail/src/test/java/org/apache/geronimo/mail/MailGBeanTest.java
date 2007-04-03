90c266d526ee121fdcc8cd48101d41c4cf234ec5
Dain Sundstrom <dain@apache.org>
2007-04-03 02:25:49 +0000

    Update to latest constructor changes in mail gbean
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@525003 13f79535-47bb-0310-9956-ffa450edef68

--- modules/geronimo-mail/src/test/java/org/apache/geronimo/mail/MailGBeanTest.java
+++ modules/geronimo-mail/src/test/java/org/apache/geronimo/mail/MailGBeanTest.java
@@ -37,7 +37,7 @@ public class MailGBeanTest extends TestCase {
         properties.put("mail.store.protocol", "testStore");
         properties.put("mail.transport.protocol", "testTransport");
 
-        MailGBean mail = new MailGBean("test:name=mail", null, Boolean.TRUE, properties, null, null, null, null, null, null);
+        MailGBean mail = new MailGBean("test:name=mail", null, Boolean.TRUE, properties, null, null, null, null, null, null, null);
         mail.doStart();
         Object proxy = mail.$getResource();
 
@@ -59,7 +59,7 @@ public class MailGBeanTest extends TestCase {
         properties.put("mail.store.protocol", "POOKIE");
         properties.put("mail.transport.protocol", "BEAR");
 
-        MailGBean mail = new MailGBean("test:name=mail", null, Boolean.TRUE, properties, null, "test", "test", null, null, null);
+        MailGBean mail = new MailGBean("test:name=mail", null, Boolean.TRUE, properties, null, "test", "test", null, null, null, null);
         mail.doStart();
         Object proxy = mail.$getResource();
 
@@ -91,7 +91,7 @@ public class MailGBeanTest extends TestCase {
         SMTPTransportGBean protocol = new SMTPTransportGBean("test:name=smtp", null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
         protocol.doStart();
 
-        MailGBean mail = new MailGBean("test:name=mail", Collections.singleton(protocol), Boolean.TRUE, properties, null, "test", "test", null, null, null);
+        MailGBean mail = new MailGBean("test:name=mail", Collections.singleton(protocol), Boolean.TRUE, properties, null, "test", "test", null, null, null, null);
         mail.doStart();
         Object proxy = mail.$getResource();
 
@@ -120,7 +120,7 @@ public class MailGBeanTest extends TestCase {
         POP3StoreGBean protocol = new POP3StoreGBean("test:name=pop3", null, null, null, null, null, null, null, null, null, null, null, null, null, null);
         protocol.doStart();
 
-        MailGBean mail = new MailGBean("test:name=mail", Collections.singleton(protocol), Boolean.TRUE, properties, null, "test", "test", null, null, null);
+        MailGBean mail = new MailGBean("test:name=mail", Collections.singleton(protocol), Boolean.TRUE, properties, null, "test", "test", null, null, null, null);
         mail.doStart();
         Object proxy = mail.$getResource();
 
@@ -146,7 +146,7 @@ public class MailGBeanTest extends TestCase {
         IMAPStoreGBean protocol = new IMAPStoreGBean("test:name=imap", null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
         protocol.doStart();
 
-        MailGBean mail = new MailGBean("test:name=mail", Collections.singleton(protocol), Boolean.TRUE, properties, null, "test", "test", null, null, null);
+        MailGBean mail = new MailGBean("test:name=mail", Collections.singleton(protocol), Boolean.TRUE, properties, null, "test", "test", null, null, null, null);
         mail.doStart();
         Object proxy = mail.$getResource();
 
