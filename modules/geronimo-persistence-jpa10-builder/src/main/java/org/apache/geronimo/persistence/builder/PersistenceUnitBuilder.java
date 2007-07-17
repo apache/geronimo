afcbcd34f5b60c4267d75dbd88a65195ddb2bd35
Donald Woods <dwoods@apache.org>
2007-07-17 18:09:23 +0000

    GERONIMO-3317 Sets PersistenceUnitGBean as the dependence of Module GBean to make sure the ClassTransformer is installed before EjbModuleImplGBean.  Song, thanks for the patch.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@557004 13f79535-47bb-0310-9956-ffa450edef68

--- modules/geronimo-persistence-jpa10-builder/src/main/java/org/apache/geronimo/persistence/builder/PersistenceUnitBuilder.java
+++ modules/geronimo-persistence-jpa10-builder/src/main/java/org/apache/geronimo/persistence/builder/PersistenceUnitBuilder.java
@@ -23,6 +23,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -46,6 +47,7 @@ import org.apache.geronimo.j2ee.deployment.Module;
 import org.apache.geronimo.j2ee.deployment.ModuleBuilderExtension;
 import org.apache.geronimo.j2ee.j2eeobjectnames.NameFactory;
 import org.apache.geronimo.kernel.GBeanAlreadyExistsException;
+import org.apache.geronimo.kernel.GBeanNotFoundException;
 import org.apache.geronimo.kernel.Naming;
 import org.apache.geronimo.kernel.config.ConfigurationStore;
 import org.apache.geronimo.kernel.repository.Environment;
@@ -178,6 +180,15 @@ public class PersistenceUnitBuilder implements ModuleBuilderExtension {
     }
 
     public void addGBeans(EARContext earContext, Module module, ClassLoader cl, Collection repository) throws DeploymentException {
+        try {
+            //make sure the PersistenceUnitGBean is started before the Module GBean
+            if (earContext.findGBeans(new AbstractNameQuery(earContext.getConfigID(), Collections.EMPTY_MAP, PersistenceUnitGBean.class.getName())).size() > 0) {
+                GBeanData moduleGBeanData = earContext.getGBeanInstance(module.getModuleName());
+                moduleGBeanData.addDependency(new AbstractNameQuery(earContext.getConfigID(), Collections.EMPTY_MAP, PersistenceUnitGBean.class.getName()));
+            }
+        } catch (GBeanNotFoundException e) {
+            //Module GBean not found, do nothing
+        }
     }
 
     private void buildPersistenceUnits(PersistenceDocument.Persistence persistence, Map<String, PersistenceDocument.Persistence.PersistenceUnit> overrides, Module module, String persistenceModulePath) throws DeploymentException {
