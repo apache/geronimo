c56378898f2b5324e820ded823862062e7cfb052
Donald Woods <dwoods@apache.org>
2007-08-18 03:01:07 +0000

    GERONIMO-3420 Remote deploy of an EAR without an application.xml plan fails
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@567217 13f79535-47bb-0310-9956-ffa450edef68

--- applications/geronimo-remote-deploy/src/main/java/org/apache/geronimo/deployment/remote/FileUploadServlet.java
+++ applications/geronimo-remote-deploy/src/main/java/org/apache/geronimo/deployment/remote/FileUploadServlet.java
@@ -29,64 +29,164 @@ import java.io.FileOutputStream;
 
 /**
  * A servlet that accepts file uploads.  It takes only POST requests, which should
- * contain a Java "DataOutput" formatted stream containing:
+ * contain a Java "DataOutput" formatted stream from RemoteDeployUtil containing:
  *
- * 1) an int, the number of files being uploaded
- * 2) for each file:
- *    1) an int, the length of the file in bytes
- *    2) a number of raw bytes equal to the above for the file
+ * RemoteDeployer data stream format:
+ *   0) an int, the version of this datastream format - REMOTE_DEPLOY_REQUEST_VER
+ *   1) an int, the number of files being uploaded
+ *   2) for each file:
+ *     2.0) a UTF String, the filename of the file being uploaded
+ *     2.1) a long, the length of the file in bytes
+ *     2.2) byte[], byte count equal to the number above for the file
  *
- * It returns a serialized stream containing:
+ * RemoteDeployer response stream format:
+ *   It returns a serialized stream containing:
+ *   0) an int, the version of this datastream format - REMOTE_DEPLOY_RESPONSE_VER
+ *   1) a UTF string, the status (should be "OK")
+ *   2) an int, the number of files received
+ *   3) for each file:
+ *     3.1) a UTF String, the path to the file as saved to the server's filesystem
  *
- * 1) a UTF string, the status (should be "OK")
- * 2) an int, the number of files received
- * 3) for each file:
- *    1) a UTF String, the path to the file as saved to the server's filesystem
- *
- * The file positions in the response will be the same as in the request.
- * The is, a name for upload file #2 will be in response position #2.
+ *   The file positions in the response will be the same as in the request.
+ *   That is, a name for upload file #2 will be in response position #2.
  *
  * @version $Rev$ $Date$
  */
 public class FileUploadServlet extends HttpServlet {
+
+    /** Note:  The below versions should be kept in sync with those in RemoteDeployUtil.java **/
+    // Starting RemoteDeploy datastream versions
+    public static final int REMOTE_DEPLOY_REQUEST_VER_0 = 0;
+    public static final int REMOTE_DEPLOY_RESPONSE_VER_0 = 0;
+    // Current RemoteDeploy datastream versions
+    public static final int REMOTE_DEPLOY_REQUEST_VER = 0;
+    public static final int REMOTE_DEPLOY_RESPONSE_VER = 0;
+
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int fileCount;
-        String names[];
+        int fileCount = 0, filesCreated = 0;
+        String names[] = null;
+        String status = "OK";
+
+        /* --------------------
+         * RemoteDeploy Request
+         * --------------------
+         *
+         * Note:  The below code has to match RemoteDeployUtil.java
+         *
+         * RemoteDeployer data stream format:
+         *   0) an int, the version of this datastream format - REMOTE_DEPLOY_REQUEST_VER
+         *   1) an int, the number of files being uploaded
+         *   2) for each file:
+         *     2.0) a UTF String, the filename of the file being uploaded
+         *     2.1) a long, the length of the file in bytes
+         *     2.2) byte[], byte count equal to the number above for the file
+         */
+        DataInputStream in = null;
         try {
-            DataInputStream in = new DataInputStream(request.getInputStream());
-            fileCount = in.readInt();
-            names = new String[fileCount];
-            for(int i=0; i<fileCount; i++) {
-                int length = in.readInt();
-                File temp = File.createTempFile("remote-deploy", "");
-                temp.deleteOnExit();
-                names[i] = temp.getAbsolutePath();
-                readToFile(in, temp, length);
+            String fileName, fileExt;
+            in = new DataInputStream(request.getInputStream());
+            // 0) an int, the version of this datastream format - REMOTE_DEPLOY_REQUEST_VER
+            int reqVer = in.readInt();
+            // whenever we update the stream version, the next line needs to
+            // be changed to just - (reqVer >= REMOTE_DEPLOY_REQUEST_VER_0)
+            // but until then, be more restrictive so we can handle old deployers
+            // that don't send a version as the first thing, but a file count instead...
+            if ((reqVer >= REMOTE_DEPLOY_REQUEST_VER_0) && (reqVer <= REMOTE_DEPLOY_REQUEST_VER)) {
+                // 1) an int, the number of files being uploaded
+                fileCount = in.readInt();
+                names = new String[fileCount];
+                // 2) for each file:
+                for(int i=0; i<fileCount; i++) {
+                    // 2.0) a UTF String, the filename of the file being uploaded
+                    fileName = in.readUTF();
+                    if (fileName != null) {
+                        int fileSep = fileName.lastIndexOf('.');
+                        if (fileSep != -1) {
+                            fileExt = fileName.substring(fileSep);
+                        } else {
+                            fileExt = "";
+                        }
+                    } else {
+                        fileExt = "";
+                    }
+                    // 2.1) a long, the length of the file in bytes
+                    long length = in.readLong();
+                    // create the local temp file
+                    File temp = File.createTempFile("remote-deploy", fileExt);
+                    temp.deleteOnExit();
+                    names[i] = temp.getAbsolutePath();
+                    // 2.2) raw bytes, equal to the number above for the file
+                    readToFile(in, temp, length);
+                    filesCreated++;
+                }
             }
-            in.close();
         } catch (IOException e) {
-            DataOutputStream out = new DataOutputStream(response.getOutputStream());
-            out.writeUTF("ERROR: "+e.getMessage());
-            out.close();
-            return;
+            status = "ERROR: "+e.getMessage();
+        } finally {
+            if (in != null) {
+                in.close();
+                in = null;
+            }
         }
-        DataOutputStream out = new DataOutputStream(response.getOutputStream());
-        out.writeUTF("OK");
-        out.writeInt(fileCount);
-        for (int i = 0; i < names.length; i++) {
-            out.writeUTF(names[i]);
+
+        /* ---------------------
+         * RemoteDeploy Response
+         * ---------------------
+         *
+         * Note:  The below code has to match RemoteDeployUtil.java
+         *
+         * RemoteDeployer response stream format:
+         *   It returns a serialized stream containing:
+         *   0) an int, the version of this datastream format - REMOTE_DEPLOY_RESPONSE_VER
+         *   1) a UTF string, the status (should be "OK")
+         *   2) an int, the number of files received
+         *   3) for each file:
+         *     3.1) a UTF String, the path to the file as saved to the server's filesystem
+         *   x) new data would be added here
+         *
+         *   The file positions in the response will be the same as in the request.
+         *   That is, a name for upload file #2 will be in response position #2.
+         */
+        DataOutputStream out = null;
+        try {
+            out = new DataOutputStream(response.getOutputStream());
+            // 0) an int, the version of this datastream format - REMOTE_DEPLOY_RESPONSE_VER
+            out.writeInt(REMOTE_DEPLOY_RESPONSE_VER);
+            // 1) a UTF string, the status (should be "OK")
+            out.writeUTF(status);
+            if (filesCreated == fileCount) {
+                // 2) an int, the number of files received
+                out.writeInt(fileCount);
+                // 3) for each file:
+                for (int i = 0; i < names.length; i++) {
+                    // 3.1) a UTF String, the path to the file as saved to the server's filesystem
+                    out.writeUTF(names[i]);
+                }
+                // x) new data would be added here
+                // only send newer data depending on the REQUEST_VER that came in
+            } else {
+                // error occurred, so don't send back any filenames, just a zero count
+                // 2) an int, the number of files received
+                out.writeInt(0);
+            }
+        } finally {
+            if (out != null) {
+                out.flush();
+                out.close();
+                out = null;
+            }
         }
-        out.flush();
-        out.close();
     }
 
-    private static void readToFile(DataInputStream in, File temp, int length) throws IOException {
+    private static void readToFile(DataInputStream in, File temp, long length) throws IOException {
         BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(temp));
-        int total, read;
+        int read;
+        long total;
         try {
-            byte[] buf = new byte[1024];
+            byte[] buf = new byte[8192];
             total = 0;
-            while((read = in.read(buf, 0, Math.min(buf.length, length - total))) > -1) {
+            while((read = in.read(buf, 0, (int)Math.min(buf.length, length - total))) > -1) {
                 out.write(buf, 0, read);
                 total += read;
                 if(total == length) {
@@ -96,9 +196,10 @@ public class FileUploadServlet extends HttpServlet {
         } finally {
             try {out.flush();} catch (IOException e) {}
             out.close();
+            out = null;
         }
         if(total != length) {
-            throw new IOException("Unable to read entire upload file ("+total+"b expecting "+length+"b)");
+            throw new IOException("Unable to read entire upload file ("+total+"B expecting "+length+"B)");
         }
     }
 }
