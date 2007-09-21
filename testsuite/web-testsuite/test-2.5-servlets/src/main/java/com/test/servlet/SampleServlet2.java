1e622f0e9f79b215eda7d2f55579ed7a69e56db6
Jarek Gawor <gawor@apache.org>
2007-09-21 03:24:09 +0000

    fixed test (restored deleted files)
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@577971 13f79535-47bb-0310-9956-ffa450edef68

---
+++ testsuite/web-testsuite/test-2.5-servlets/src/main/java/com/test/servlet/SampleServlet2.java
@@ -0,0 +1,74 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.test.servlet;
+
+import java.io.*;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class for Servlet: SampleServlet
+ *
+ * @web.servlet
+ *   name="SampleServlet"
+ *   display-name="SampleServlet"
+ *
+ * @web.servlet-mapping
+ *   url-pattern="/SampleServlet"
+ *
+ */
+ public class  SampleServlet2 extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
+    /* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#HttpServlet()
+	 */
+	public  SampleServlet2() {
+		super();
+
+	}
+
+	/* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	   doPost(request,response);
+	}
+
+	/* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title>Sample application with Servlets 2.5</title></head></html>");
+		out.println("<body>");
+		out.println("<font align=Center face=\"Garamond\">Welcome to Servlets 2.5 samples.</font>");
+		out.println("<font align=Center face=\"Garamond\">Another Sample Servlet!</font>");
+		out.println("</body></html>");
+
+	}
+	
+	public void init(ServletConfig config)
+	{
+		System.out.println("Initializing servlet");
+	}
+	
+}
