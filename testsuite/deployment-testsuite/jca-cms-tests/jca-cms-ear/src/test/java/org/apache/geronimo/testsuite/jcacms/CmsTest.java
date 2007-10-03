228e6c6d5921005915f7b5cbe0cbf720e51733a5
Jarek Gawor <gawor@apache.org>
2007-10-03 18:46:20 +0000

    fixed the jca-cms-tests. added another login module that actaully validates the credentials
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/branches/2.0@581682 13f79535-47bb-0310-9956-ffa450edef68

--- testsuite/deployment-testsuite/jca-cms-tests/jca-cms-ear/src/test/java/org/apache/geronimo/testsuite/jcacms/CmsTest.java
+++ testsuite/deployment-testsuite/jca-cms-tests/jca-cms-ear/src/test/java/org/apache/geronimo/testsuite/jcacms/CmsTest.java
@@ -32,12 +32,53 @@ public class CmsTest
     extends SeleniumTestSupport
 {
     @Test
-    public void testPageContent() throws Exception {
-        selenium.open("http://system:manager@localhost:8080/jca-cms/servlet");
+    public void testPageContent1() throws Exception {
+        selenium.open("http://localhost:8080/jca-cms/default-subject-servlet");
         selenium.waitForPageToLoad("30000");
 
-        //TODO the line separators might well be wrong???
-        assertEquals("Successfully got configured connection\n\nSuccessfully got container managed connection\n", selenium.getText("xpath=/html/body"));
+        String body = selenium.getText("xpath=/html/body");
+
+        assertTrue(body.endsWith(
+//                "Current subject: Subject:\n" +
+//                "\tPrincipal: org.apache.geronimo.connector.outbound.security.ResourcePrincipal@cb1c722f\n" +
+//                "\tPrincipal: org.apache.geronimo.security.IdentificationPrincipal[[1186174499145:0x607c7eb7837eabcd6b759e6f9d29e7eee72622d6]]\n" +
+//                "\tPrincipal: org.apache.geronimo.security.IdentificationPrincipal[[1186174499146:0x7622e0831ed59a4cd6c277a53491de74f1489311]]\n" +
+//                "\tPrivate Credential: javax.resource.spi.security.PasswordCredential@23f33e5c\n" +
+//                "\n" +
+//                "Next subject:    Subject:\n" +
+//                "\tPrincipal: org.apache.geronimo.connector.outbound.security.ResourcePrincipal@cb1c722f\n" +
+//                "\tPrincipal: org.apache.geronimo.security.IdentificationPrincipal[[1186174499145:0x607c7eb7837eabcd6b759e6f9d29e7eee72622d6]]\n" +
+//                "\tPrincipal: org.apache.geronimo.security.IdentificationPrincipal[[1186174499146:0x7622e0831ed59a4cd6c277a53491de74f1489311]]\n" +
+//                "\tPrivate Credential: javax.resource.spi.security.PasswordCredential@23f33e5c\n" +
+//                "\n" +
+                "Successfully got configured connection\n" +
+                "\n" +
+                "Successfully got container managed connection"));
+
+        String expectedPrincipal = "Principal: george";
+        int pos1 = body.indexOf(expectedPrincipal);
+        assertTrue("Expected current subject principal", pos1 > 0);
+        int pos2 = body.indexOf(expectedPrincipal, pos1 + expectedPrincipal.length());
+        assertTrue("Expected next subject principal", pos2 > 0);
+    }
+
+    @Test
+    public void testPageContent2() throws Exception {
+        selenium.open("http://localhost:8080/jca-cms/run-as-servlet");
+        selenium.waitForPageToLoad("30000");
+
+        String body = selenium.getText("xpath=/html/body");
+
+        assertTrue(body.endsWith(
+                "Successfully got configured connection\n" +
+                "\n" +
+                "Successfully got container managed connection"));
+
+        String expectedPrincipal = "Principal: george";
+        int pos1 = body.indexOf(expectedPrincipal);
+        assertTrue("Expected current subject principal", pos1 > 0);
+        int pos2 = body.indexOf("Principal: gracie", pos1 + expectedPrincipal.length());
+        assertTrue("Expected next subject principal", pos2 > 0);
     }
 }
 
