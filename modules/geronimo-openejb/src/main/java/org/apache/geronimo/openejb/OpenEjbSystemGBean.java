0e7bf418cbcfb7383e537eacc510a7e8f84b73b8
David Blevins <dblevins@apache.org>
2007-10-03 01:55:44 +0000

    Accidentally checked it in in a non-configurable way.  Made it configurable.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/branches/2.0@581470 13f79535-47bb-0310-9956-ffa450edef68

--- modules/geronimo-openejb/src/main/java/org/apache/geronimo/openejb/OpenEjbSystemGBean.java
+++ modules/geronimo-openejb/src/main/java/org/apache/geronimo/openejb/OpenEjbSystemGBean.java
@@ -107,12 +107,12 @@ public class OpenEjbSystemGBean implements OpenEjbSystem {
         System.setProperty("duct tape","");
         System.setProperty("admin.disabled", "true");
         System.setProperty("openejb.logger.external", "true");
-        System.setProperty("openejb.jndiname.failoncollision", "false");
 
         setDefaultProperty("openejb.deploymentId.format", "{appId}/{moduleId}/{ejbName}");
         setDefaultProperty("openejb.jndiname.strategy.class", "org.apache.openejb.assembler.classic.JndiBuilder$TemplatedStrategy");
         setDefaultProperty("openejb.jndiname.format", "{ejbName}{interfaceType.annotationName}");
-
+        setDefaultProperty("openejb.jndiname.failoncollision", "false");
+        
         System.setProperty("openejb.naming", "xbean");
         if (transactionManager == null) {
             throw new NullPointerException("transactionManager is null");
