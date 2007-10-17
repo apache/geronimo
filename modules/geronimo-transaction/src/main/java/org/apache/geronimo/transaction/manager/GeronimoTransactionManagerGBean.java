e592e45296588cdd2b9d470ea06364731e70f754
Anita Kulshreshtha <akulshreshtha@apache.org>
2007-10-17 15:31:04 +0000

    GERONIMO-3520 Make TransactionManager a Statistics Provider. Patch by Viet H. Nguyen
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@585537 13f79535-47bb-0310-9956-ffa450edef68

--- modules/geronimo-transaction/src/main/java/org/apache/geronimo/transaction/manager/GeronimoTransactionManagerGBean.java
+++ modules/geronimo-transaction/src/main/java/org/apache/geronimo/transaction/manager/GeronimoTransactionManagerGBean.java
@@ -43,6 +43,7 @@ public class GeronimoTransactionManagerGBean extends GeronimoTransactionManager 
                 xidFactory,
                 transactionLog
         );
+        stats.setStartTime();
     }
 
 
