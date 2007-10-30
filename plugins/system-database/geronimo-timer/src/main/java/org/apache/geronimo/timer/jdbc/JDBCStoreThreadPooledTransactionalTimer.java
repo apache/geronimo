efcfdc7b8beb2e63f32fd14684d59a31de2818be
Prasad Kashyap <prasad@apache.org>
2007-10-30 19:58:12 +0000

    restructuring trunk
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@590348 13f79535-47bb-0310-9956-ffa450edef68

---
+++ plugins/system-database/geronimo-timer/src/main/java/org/apache/geronimo/timer/jdbc/JDBCStoreThreadPooledTransactionalTimer.java
@@ -0,0 +1,73 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.geronimo.timer.jdbc;
+
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import javax.transaction.TransactionManager;
+import javax.resource.ResourceException;
+
+import java.util.concurrent.Executor;
+
+import org.apache.geronimo.connector.outbound.ConnectionFactorySource;
+import org.apache.geronimo.gbean.GBeanInfo;
+import org.apache.geronimo.gbean.GBeanInfoBuilder;
+import org.apache.geronimo.j2ee.j2eeobjectnames.NameFactory;
+import org.apache.geronimo.kernel.Kernel;
+import org.apache.geronimo.timer.PersistentTimer;
+import org.apache.geronimo.timer.ThreadPooledTimer;
+import org.apache.geronimo.timer.TransactionalExecutorTaskFactory;
+
+/**
+ *
+ *
+ * @version $Rev$ $Date$
+ *
+ */
+public class JDBCStoreThreadPooledTransactionalTimer extends ThreadPooledTimer {
+
+    public JDBCStoreThreadPooledTransactionalTimer(int repeatCount,
+            TransactionManager transactionManager,
+            ConnectionFactorySource managedConnectionFactoryWrapper,
+            Executor threadPool,
+            Kernel kernel) throws SQLException, ResourceException {
+        super(new TransactionalExecutorTaskFactory(transactionManager, repeatCount),
+                new JDBCWorkerPersistence(kernel.getKernelName(), (DataSource)managedConnectionFactoryWrapper.$getResource(), false), threadPool, transactionManager);
+    }
+
+
+    public static final GBeanInfo GBEAN_INFO;
+
+    static {
+        GBeanInfoBuilder infoFactory = GBeanInfoBuilder.createStatic(JDBCStoreThreadPooledTransactionalTimer.class);
+        infoFactory.addInterface(PersistentTimer.class);
+
+        infoFactory.addAttribute("repeatCount", int.class, true);
+        infoFactory.addReference("TransactionManager", TransactionManager.class, NameFactory.JTA_RESOURCE);
+        infoFactory.addReference("ManagedConnectionFactoryWrapper", ConnectionFactorySource.class, NameFactory.JCA_MANAGED_CONNECTION_FACTORY);
+        infoFactory.addReference("ThreadPool", Executor.class, NameFactory.GERONIMO_SERVICE);
+        infoFactory.addAttribute("kernel", Kernel.class, false);
+
+        infoFactory.setConstructor(new String[] {"repeatCount", "TransactionManager", "ManagedConnectionFactoryWrapper", "ThreadPool", "kernel"});
+        GBEAN_INFO = infoFactory.getBeanInfo();
+    }
+
+    public static GBeanInfo getGBeanInfo() {
+        return GBEAN_INFO;
+    }
+}
