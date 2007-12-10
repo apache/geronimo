da6a5ba667d20b3a4d528cddb9f5ebc0b67b47ce
David Blevins <dblevins@apache.org>
2007-12-10 08:09:44 +0000

    Updated for changes on OpenEJB side
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@602805 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/openejb/geronimo-openejb-builder/src/main/java/org/apache/geronimo/openejb/deployment/EjbRefBuilder.java
+++ plugins/openejb/geronimo-openejb-builder/src/main/java/org/apache/geronimo/openejb/deployment/EjbRefBuilder.java
@@ -53,7 +53,9 @@ import org.apache.openejb.OpenEJBException;
 import org.apache.openejb.assembler.classic.EjbJarInfo;
 import org.apache.openejb.assembler.classic.JndiEncBuilder;
 import org.apache.openejb.assembler.classic.JndiEncInfo;
+import org.apache.openejb.assembler.classic.AppInfo;
 import org.apache.openejb.config.JndiEncInfoBuilder;
+import org.apache.openejb.config.AppModule;
 import org.apache.openejb.core.ivm.naming.IntraVmJndiReference;
 import org.apache.openejb.jee.EjbLocalRef;
 import org.apache.openejb.jee.EjbRef;
@@ -140,7 +142,12 @@ public class EjbRefBuilder extends AbstractNamingBuilder {
             if (earData != null) {
                 ejbJars = earData.getEjbJars();
             }
-            JndiEncInfoBuilder jndiEncInfoBuilder = new JndiEncInfoBuilder(ejbJars);
+
+
+            AppInfo appInfo = new AppInfo();
+            appInfo.ejbJars.addAll(ejbJars);
+
+            JndiEncInfoBuilder jndiEncInfoBuilder = new JndiEncInfoBuilder(appInfo);
             JndiEncInfo jndiEncInfo;
             if (module.isStandAlone()) {
                 jndiEncInfo = jndiEncInfoBuilder.build(consumer, "GeronimoEnc", null);
