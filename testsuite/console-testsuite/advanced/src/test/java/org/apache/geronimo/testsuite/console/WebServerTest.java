636bd0b2b0b4b84ebe70f67368fc292835aa1f51
Prasad Kashyap <prasad@apache.org>
2007-12-13 15:12:17 +0000

    * edit is now the 3rd link in the row
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@603934 13f79535-47bb-0310-9956-ffa450edef68

--- testsuite/console-testsuite/advanced/src/test/java/org/apache/geronimo/testsuite/console/WebServerTest.java
+++ testsuite/console-testsuite/advanced/src/test/java/org/apache/geronimo/testsuite/console/WebServerTest.java
@@ -52,7 +52,7 @@ public class WebServerTest extends ConsoleTestSupport {
 
             String connectorSelector = "//tr[td[1] = \"" + name + "\"]";
             
-            selenium.click(connectorSelector + "/td[5]/a[2]");        
+            selenium.click(connectorSelector + "/td[5]/a[3]");        
             selenium.waitForPageToLoad("30000");
 
             selenium.type("port", "8008");
@@ -60,7 +60,7 @@ public class WebServerTest extends ConsoleTestSupport {
             selenium.waitForPageToLoad("30000");
             assertTrue(selenium.isTextPresent("8008"));
             
-            selenium.click(connectorSelector + "/td[5]/a[2]"); 
+            selenium.click(connectorSelector + "/td[5]/a[3]"); 
 
             selenium.waitForPageToLoad("30000");
             selenium.type("port", "8009");
