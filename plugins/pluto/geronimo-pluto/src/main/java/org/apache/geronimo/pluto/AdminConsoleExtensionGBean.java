840f8573d535cf96e553a25eb09aa2511f07aed5
David Jencks <djencks@apache.org>
2008-01-10 23:20:43 +0000

    GERONIMO-3745 clean up AdminConsoleExtensionGBean
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@610984 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/pluto/geronimo-pluto/src/main/java/org/apache/geronimo/pluto/AdminConsoleExtensionGBean.java
+++ plugins/pluto/geronimo-pluto/src/main/java/org/apache/geronimo/pluto/AdminConsoleExtensionGBean.java
@@ -18,7 +18,6 @@ package org.apache.geronimo.pluto;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
@@ -26,8 +25,6 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.geronimo.gbean.GBeanInfo;
 import org.apache.geronimo.gbean.GBeanInfoBuilder;
 import org.apache.geronimo.gbean.GBeanLifecycle;
-import org.apache.geronimo.kernel.Kernel;
-import org.apache.geronimo.kernel.KernelRegistry;
 import org.apache.pluto.driver.services.portal.PageConfig;
 import org.apache.pluto.driver.services.portal.PortletWindowConfig;
 import org.apache.pluto.driver.services.portal.RenderConfigService;
@@ -43,46 +40,45 @@ import org.apache.pluto.driver.services.portal.admin.RenderConfigAdminService;
  *    <attribute name="portletList">[portlet_id_1, portlet_id_2]</attribute>
  *  </gbean>
  */
-public class AdminConsoleExtensionGBean implements GBeanLifecycle, PlutoAccessInterface, AdminConsoleExtension {
+public class AdminConsoleExtensionGBean implements GBeanLifecycle {
     private static final Log log = LogFactory.getLog(AdminConsoleExtensionGBean.class);
     public static final GBeanInfo GBEAN_INFO;
-    private String pageTitle;
-    private String portletContext;
-    private ArrayList<String> portletList;
+    private final String pageTitle;
+    private final String portletContext;
+    private final ArrayList<String> portletList;
+    private final PortalContainerServices portletContainerServices;
 
     /* Constructor for GBean */
-    public AdminConsoleExtensionGBean(String pageTitle, String portletContext, ArrayList<String> portletList) {
+    public AdminConsoleExtensionGBean(String pageTitle, String portletContext, ArrayList<String> portletList, PortalContainerServices portalContainerServices) {
         super();
         this.pageTitle = pageTitle;
         this.portletContext = portletContext;
         this.portletList = portletList;
+        this.portletContainerServices = portalContainerServices;
     }
 
     /*
      * Add the PageConfig to pluto.  This will overwrite any existing pages with the same page name
      * @see org.apache.geronimo.pluto.PlutoAccessInterface#addPage(org.apache.pluto.driver.services.portal.PageConfig)
      */
-    public void addPage(PageConfig pageConfig) {
-        //Get the handle on the pluto service
-        PortalContainerServices container = getPortalContainerServices();
-
+    private void addPage(PageConfig pageConfig) {
         //Add the new PageConfig to Pluto
-        RenderConfigAdminService renderConfig = container.getAdminConfiguration().getRenderConfigAdminService();
+        RenderConfigAdminService renderConfig = portletContainerServices.getAdminConfiguration().getRenderConfigAdminService();
         renderConfig.addPage(pageConfig);
     }
-    
+
     /*
-     * This will add the portlets to the PageConfig in Pluto.
-     * @see org.apache.geronimo.pluto.PlutoAccessInterface#addPortlets(java.lang.String, java.lang.String, java.util.ArrayList)
-     */
-    public void addPortlets(String pageTitle, String portletContext, ArrayList<String> portletList) {
-        if(pageExists(pageTitle)){
-            PageConfig pageConfig = getPageConfigFromPluto(pageTitle);
+    * This will add the portlets to the PageConfig in Pluto.
+    * @see org.apache.geronimo.pluto.PlutoAccessInterface#addPortlets(java.lang.String, java.lang.String, java.util.ArrayList)
+    */
+    private void addPortlets() {
+        if (pageExists()) {
+            PageConfig pageConfig = getPageConfigFromPluto();
             int portletCount = portletList.size();
-            for (int i=0; i<portletCount; i++){
-                pageConfig.addPortlet(portletContext,portletList.get(i));
+            for (int i = 0; i < portletCount; i++) {
+                pageConfig.addPortlet(portletContext, portletList.get(i));
             }
-        }else{
+        } else {
             log.warn("Cannot add portlets to non-existent page " + pageTitle);
         }
     }
@@ -91,15 +87,12 @@ public class AdminConsoleExtensionGBean implements GBeanLifecycle, PlutoAccessIn
      * Removes a PageConfig object in Pluto with the pageTitle
      * @see org.apache.geronimo.pluto.PlutoAccessInterface#removePage(java.lang.String)
      */
-    public void removePage(String pageTitle) {
+    private void removePage() {
         //all we really need is a PageConfig with the page name
         PageConfig pageConfig = createPageConfig();
-        
-        //Get the handle on the pluto service
-        PortalContainerServices container = getPortalContainerServices();
-        
-        RenderConfigAdminService renderConfig = container.getAdminConfiguration().getRenderConfigAdminService();
-        
+
+        RenderConfigAdminService renderConfig = portletContainerServices.getAdminConfiguration().getRenderConfigAdminService();
+
         //This removePage method was added into Pluto as a patch (PLUTO-387). addPage functionality
         //was available, but removePage functionality was unavailable.
         //NOTE: getList returns a copy of the Map that stores page data in List form. Since it's a copy,
@@ -107,134 +100,107 @@ public class AdminConsoleExtensionGBean implements GBeanLifecycle, PlutoAccessIn
         //solution was just to try to patch the code.
         renderConfig.removePage(pageConfig);
     }
-    
+
     /*
-     * Removes the portletList from the PageConfig in Pluto
-     * @see org.apache.geronimo.pluto.PlutoAccessInterface#removePortlets(java.lang.String, java.util.ArrayList)
-     */
-    public void removePortlets(String pageTitle, ArrayList<String> portletList) {
-        if(pageExists(pageTitle)) {
-            PageConfig pageConfig = getPageConfigFromPluto(pageTitle);
+    * Removes the portletList from the PageConfig in Pluto
+    * @see org.apache.geronimo.pluto.PlutoAccessInterface#removePortlets(java.lang.String, java.util.ArrayList)
+    */
+    private void removePortlets() {
+        if (pageExists()) {
+            PageConfig pageConfig = getPageConfigFromPluto();
             int portletCount = portletList.size();
-            Collection list = pageConfig.getPortletIds();
-            
+            Collection<String> list = pageConfig.getPortletIds();
+
             //run through the list of portlets to remove
-            for (int i=0; i<portletCount; i++){
+            for (int i = 0; i < portletCount; i++) {
                 String portletName = portletList.get(i);
-                
+
                 //run through the list of portlets on this page and see if we can find a match
-                Iterator it = list.iterator();
-                while(it.hasNext()){
-                    String pid = (String) it.next();
+                for (String pid : list) {
                     String pletContext = PortletWindowConfig.parseContextPath(pid);
                     String pletName = PortletWindowConfig.parsePortletName(pid);
-                    if(portletContext.equals(pletContext) && portletName.equals(pletName)){
+                    if (portletContext.equals(pletContext) && portletName.equals(pletName)) {
                         pageConfig.removePortlet(pid);
                         break;
                     }
                 }
             }
-        }else{
+        } else {
             log.warn("can't remove portlets from non-existent page " + pageTitle);
         }
-    }
-    
-    /*
-     * Gets the PortalContainerServices object from Geronimo
-     *  depends on the the PortalContainerServicesGBean
-     */
-    private PortalContainerServices getPortalContainerServices(){
-        Kernel kernel = KernelRegistry.getSingleKernel();     
-        PortalContainerServices container = null;    
-        try {      
-            container = (PortalContainerServices) kernel.getGBean(PortalContainerServices.class);   
-        } catch (Exception e) {
-            //If we fall into here, the likely (but not only) cause is that we couldn't find the
-            //PortalContainerService in the Kernel.
-            log.error("Cannot get the portal container services", e);
-        }
-        return container;
     }
 
     /*
      * Creates a new PageConfig object with this GBean's pageTitle
      */
-    private PageConfig createPageConfig(){
+    private PageConfig createPageConfig() {
         //Create a new PageConfig
         PageConfig pageConfig = new PageConfig();
         pageConfig.setName(pageTitle);
         pageConfig.setUri("/WEB-INF/themes/default-theme.jsp");
         return pageConfig;
     }
-    
+
     /*
-     * Gets the PageConfig object from Pluto with that name
-     */
-    private PageConfig getPageConfigFromPluto(String pageTitle){
-        //Get the handle on the pluto service
-        PortalContainerServices container = getPortalContainerServices();
-        RenderConfigService service = container.getRenderConfigService();
-        
-        PageConfig pageConfig = service.getPage(pageTitle);
-        return pageConfig;
+    * Gets the PageConfig object from Pluto with that name
+    */
+    private PageConfig getPageConfigFromPluto() {
+        RenderConfigService service = portletContainerServices.getRenderConfigService();
+
+        return service.getPage(pageTitle);
     }
-    
+
     /*
-     * return true if Pluto contains a PageConfig with that name, else false
-     */
-    private boolean pageExists(String pageTitle){
+    * return true if Pluto contains a PageConfig with that name, else false
+    */
+    private boolean pageExists() {
         if (pageTitle == null) return false;
-        
-        //Get the handle on pluto's rendering service
-        PortalContainerServices container = getPortalContainerServices();
-        RenderConfigService service = container.getRenderConfigService();
-        
+
+        RenderConfigService service = portletContainerServices.getRenderConfigService();
+
         List<PageConfig> pageConfigs = service.getPages();
         for (PageConfig pageConfig : pageConfigs) {
             if (pageTitle.equals(pageConfig.getName())) {
                 return true;
             }
         }
-        
+
         return false;
     }
-    
+
     /*
-     * returns true if Pluto contains a PageConfig with that name and it has 0 or if there isn't a page with that name
-     */
-    private boolean pageIsEmpty(String pageTitle){
-        if(pageExists(pageTitle)){
-            PageConfig pageConfig = getPageConfigFromPluto(pageTitle);
-            return pageConfig.getPortletIds().size() > 0 ? false : true;
-        }else{
+    * returns true if Pluto contains a PageConfig with that name and it has 0 or if there isn't a page with that name
+    */
+    private boolean pageIsEmpty() {
+        if (pageExists()) {
+            PageConfig pageConfig = getPageConfigFromPluto();
+            return pageConfig.getPortletIds().size() == 0;
+        } else {
             log.debug("no pageConfig found for " + pageTitle);
             return true;
         }
     }
-    
+
     /*
-     * Called when the GBean is started
-     *   This adds/updates a Page in Pluto according to this GBean's specifications (ACE)
-     * @see org.apache.geronimo.gbean.GBeanLifecycle#doStart()
-     */
-    public synchronized void doStart() throws Exception{
-        //Get the handle on the pluto service
-        PortalContainerServices container = getPortalContainerServices();
-        
+    * Called when the GBean is started
+    *   This adds/updates a Page in Pluto according to this GBean's specifications (ACE)
+    * @see org.apache.geronimo.gbean.GBeanLifecycle#doStart()
+    */
+    public synchronized void doStart() throws Exception {
         // check to make sure that a portal driver has registered with the container services
-        if (container.getAdminConfiguration() == null) {
-            throw new RuntimeException ("No portal driver has been registered with the portal container services");
+        if (portletContainerServices.getAdminConfiguration() == null) {
+            throw new RuntimeException("No portal driver has been registered with the portal container services");
         }
-        
+
         //add the page if it doesn't exist yet
-        if(!pageExists(pageTitle)){
+        if (!pageExists()) {
             //create a PageConfig
             PageConfig newPageConfig = createPageConfig();
             addPage(newPageConfig);
         }
-        
+
         //add portlets to this newly created page
-        addPortlets(pageTitle, portletContext, portletList);
+        addPortlets();
         log.debug("Started AdminConsoleExtensionGBean for " + pageTitle);
     }
 
@@ -243,14 +209,14 @@ public class AdminConsoleExtensionGBean implements GBeanLifecycle, PlutoAccessIn
      *   This removes/updates a Page in Pluto according to this GBean's specifications
      * @see org.apache.geronimo.gbean.GBeanLifecycle#doStop()
      */
-    public synchronized void doStop() throws Exception{
+    public synchronized void doStop() throws Exception {
         try {
             //remove portlets from the page
-            removePortlets(pageTitle, portletList);
-            
+            removePortlets();
+
             //if the page has 0 portlets on it, then go ahead and remove it
-            if(pageIsEmpty(pageTitle)){
-                removePage(pageTitle);
+            if (pageIsEmpty()) {
+                removePage();
             }
         } catch (NullPointerException e) {
             // during normal server shutdown the portal driver has been shut down before
@@ -260,33 +226,35 @@ public class AdminConsoleExtensionGBean implements GBeanLifecycle, PlutoAccessIn
             // way to check to see if a service has been shut down.
             log.debug("could not remove portlets for " + pageTitle, e);
         }
-        
+
         log.debug("Stopped AdminConsoleExtensionGBean for " + pageTitle);
     }
-    
+
     /*
-     * Called when the GBean fails
-     * @see org.apache.geronimo.gbean.GBeanLifecycle#doFail()
-     */
+    * Called when the GBean fails
+    * @see org.apache.geronimo.gbean.GBeanLifecycle#doFail()
+    */
     public synchronized void doFail() {
         log.warn("AdminConsoleExtensionGBean for " + pageTitle + " failed.");
     }
-    
+
     /*
-     * Standard GBean information
-     */
+    * Standard GBean information
+    */
     static {
         GBeanInfoBuilder infoFactory = new GBeanInfoBuilder("AdminConsoleExtensionGBean", AdminConsoleExtensionGBean.class);
-        infoFactory.addInterface(AdminConsoleExtension.class);
         infoFactory.addAttribute("pageTitle", String.class, true, true);
         infoFactory.addAttribute("portletContext", String.class, true, true);
         infoFactory.addAttribute("portletList", ArrayList.class, true, true);
-        infoFactory.setConstructor(new String[] { 
-                "pageTitle", 
-                "portletContext", 
-                "portletList"});
+        infoFactory.addReference("PortalContainerServices", PortalContainerServices.class, "GBean");
+        infoFactory.setConstructor(new String[]{
+                "pageTitle",
+                "portletContext",
+                "portletList",
+                "PortalContainerServices"});
         GBEAN_INFO = infoFactory.getBeanInfo();
     }
+
     public static GBeanInfo getGBeanInfo() {
         return GBEAN_INFO;
     }
