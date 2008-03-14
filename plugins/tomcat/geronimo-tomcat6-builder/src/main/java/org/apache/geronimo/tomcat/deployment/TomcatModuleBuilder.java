911b74a4e8487ea89937350431065ed2a00baa7b
Jay McHugh <jaydm@apache.org>
2008-03-14 19:39:00 +0000

    GERONIMO-3921 getContextRoot() should return "" for apps deployed to the root context.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@637223 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/tomcat/geronimo-tomcat6-builder/src/main/java/org/apache/geronimo/tomcat/deployment/TomcatModuleBuilder.java
+++ plugins/tomcat/geronimo-tomcat6-builder/src/main/java/org/apache/geronimo/tomcat/deployment/TomcatModuleBuilder.java
@@ -264,9 +264,13 @@ public class TomcatModuleBuilder extends AbstractWebModuleBuilder implements GBe
             contextRoot = determineDefaultContextRoot(webApp, standAlone, moduleFile, targetPath);
         }
         contextRoot = contextRoot.trim();
-        if (!contextRoot.startsWith("/")) {
-            //I'm not sure if we should always fix up peculiar context roots.
-            contextRoot = "/" + contextRoot;
+        if (contextRoot.length > 0) {
+            // only force the context root to start with a forward slash
+            // if it is not null
+            if (!contextRoot.startsWith("/")) {
+                //I'm not sure if we should always fix up peculiar context roots.
+                contextRoot = "/" + contextRoot;
+            }
         }
         return contextRoot;
     }
