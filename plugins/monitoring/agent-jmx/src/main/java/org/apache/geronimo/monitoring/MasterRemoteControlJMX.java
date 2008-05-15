f5e8780ebe6e166e9b4be5f0d7cd3004152d9802
David Jencks <djencks@apache.org>
2008-05-15 09:19:17 +0000

    GERONIMO-4014 clean up monitoring plugin a little bit.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@656565 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/monitoring/agent-jmx/src/main/java/org/apache/geronimo/monitoring/MasterRemoteControlJMX.java
+++ plugins/monitoring/agent-jmx/src/main/java/org/apache/geronimo/monitoring/MasterRemoteControlJMX.java
@@ -101,8 +101,8 @@ public class MasterRemoteControlJMX implements GBeanLifecycle {
         // Note: do not put this in the constructor...datasources are not injected by then
         try {
             InitialContext ic = new InitialContext();
-            activeDS = (DataSource)((ManagedConnectionFactoryWrapper)ic.lookup("jca:/org.apache.geronimo.plugins/agent-ds/JCAManagedConnectionFactory/jdbc/ActiveDS")).$getResource();
-            archiveDS = (DataSource)((ManagedConnectionFactoryWrapper)ic.lookup("jca:/org.apache.geronimo.plugins/agent-ds/JCAManagedConnectionFactory/jdbc/ArchiveDS")).$getResource();
+            activeDS = (DataSource)ic.lookup("jca:/org.apache.geronimo.plugins/agent-ds/JCAManagedConnectionFactory/jdbc/ActiveDS");
+            archiveDS = (DataSource)ic.lookup("jca:/org.apache.geronimo.plugins/agent-ds/JCAManagedConnectionFactory/jdbc/ArchiveDS");
         } catch(Exception e) {
             log.error(e.getMessage());
             e.printStackTrace();
