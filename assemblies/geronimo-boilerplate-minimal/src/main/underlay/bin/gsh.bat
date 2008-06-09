441f08b90c275898b6474357d4015ad3333cf653
Donald Woods <dwoods@apache.org>
2008-06-09 16:00:31 +0000

    GERONIMO-4093 Enhance GShell based scripts to use setjavaenv for determining the Java runtime, allow users to continue to supply a setenv script and updated the Unix gsh script with some missing Cygwin, OS400 and error checking that was in geronimo.sh.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/branches/2.1@665729 13f79535-47bb-0310-9956-ffa450edef68

--- assemblies/geronimo-boilerplate-minimal/src/main/underlay/bin/gsh.bat
+++ assemblies/geronimo-boilerplate-minimal/src/main/underlay/bin/gsh.bat
@@ -1,82 +1,86 @@
-@if "%DEBUG%" == "" @echo off
-rem 
-rem Licensed to the Apache Software Foundation (ASF) under one
-rem or more contributor license agreements.  See the NOTICE file
-rem distributed with this work for additional information
-rem regarding copyright ownership.  The ASF licenses this file
-rem to you under the Apache License, Version 2.0 (the
-rem "License"); you may not use this file except in compliance
-rem with the License.  You may obtain a copy of the License at
-rem 
-rem  http://www.apache.org/licenses/LICENSE-2.0
-rem 
-rem Unless required by applicable law or agreed to in writing,
-rem software distributed under the License is distributed on an
-rem "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-rem KIND, either express or implied.  See the License for the
-rem specific language governing permissions and limitations
-rem under the License.
-rem 
+@rem 
+@rem Licensed to the Apache Software Foundation (ASF) under one
+@rem or more contributor license agreements.  See the NOTICE file
+@rem distributed with this work for additional information
+@rem regarding copyright ownership.  The ASF licenses this file
+@rem to you under the Apache License, Version 2.0 (the
+@rem "License"); you may not use this file except in compliance
+@rem with the License.  You may obtain a copy of the License at
+@rem 
+@rem  http://www.apache.org/licenses/LICENSE-2.0
+@rem 
+@rem Unless required by applicable law or agreed to in writing,
+@rem software distributed under the License is distributed on an
+@rem "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+@rem KIND, either express or implied.  See the License for the
+@rem specific language governing permissions and limitations
+@rem under the License.
+@rem 
 
-rem 
-rem $Rev$ $Date$
-rem 
+@rem 
+@rem $Rev$ $Date$
+@rem 
 
-if "%OS%"=="Windows_NT" setlocal enableextensions
-set ERRORLEVEL=0
+@if "%GERONIMO_BATCH_ECHO%" == "on"  echo on
+@if not "%GERONIMO_BATCH_ECHO%" == "on"  echo off
 
-:begin
+if "%OS%" == "Windows_NT" goto okOsCheck
+echo Cannot process command - you are running an unsupported operating system.
+set ERRORLEVEL=1
+goto end
+
+:okOsCheck
+@setlocal enableextensions
+@set ERRORLEVEL=0
 
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.\
+cd /d %DIRNAME%
 
-:check_JAVACMD
-if not "%JAVACMD%" == "" goto check_GSHELL_HOME
+@REM Get standard environment variables
+@REM Users can optionally create this file to set environment variables.
+if exist "%DIRNAME%\setenv.bat" call "%DIRNAME%\setenv.bat"
+if not %errorlevel% == 0 goto end
 
-:check_JAVA_HOME
-if not "%JAVA_HOME%" == "" goto have_JAVA_HOME
-set JAVACMD=java
-goto check_GSHELL_HOME
-
-:have_JAVA_HOME
-set JAVACMD=%JAVA_HOME%\bin\java
-goto check_GSHELL_HOME
+@REM Get standard Java environment variables
+if exist "%DIRNAME%\setjavaenv.bat" goto okSetJavaEnv
+echo ERROR - Cannot find %DIRNAME%\setjavaenv.bat
+set ERRORLEVEL=1
+goto end
+:okSetJavaEnv
+set BASEDIR=%DIRNAME%
+call "%DIRNAME%\setJavaEnv.bat"
+if not %errorlevel% == 0 goto end
 
 :check_GSHELL_HOME
 if "%GSHELL_HOME%" == "" set GSHELL_HOME=%DIRNAME%..
 
 :init
-rem Get command-line arguments, handling Windowz variants
-if not "%OS%" == "Windows_NT" goto win9xME_args
+rem Get command-line arguments, handling Windows variants
 if "%eval[2+2]" == "4" goto 4NT_args
 
 rem Regular WinNT shell
-set ARGS=%*
-goto execute
-
-:win9xME_args
-rem Slurp the command line arguments.  This loop allows for an unlimited number
 set ARGS=
-
-:win9xME_args_slurp
-if "x%1" == "x" goto execute
+:setArgs
+if ""%1""=="""" goto doneSetArgs
 set ARGS=%ARGS% %1
 shift
-goto win9xME_args_slurp
+goto setArgs
+:doneSetArgs
+goto execute
 
 :4NT_args
 rem Get arguments from the 4NT Shell from JP Software
 set ARGS=%$
 
 :execute
-
 set BOOTJAR=%GSHELL_HOME%\lib\boot\gshell-bootstrap.jar
 
 rem Start the JVM
-"%JAVACMD%" %JAVA_OPTS% -jar "%BOOTJAR%" %ARGS%
+%_RUNJAVA% %JAVA_OPTS% -jar "%BOOTJAR%" %ARGS%
 
 :end
-
-if "%OS%"=="Windows_NT" endlocal
-if "%GSHELL_BATCH_PAUSE%" == "on" pause
-
+:end
+@REM pause the batch file if GERONIMO_BATCH_PAUSE is set to 'on'
+if "%GERONIMO_BATCH_PAUSE%" == "on" pause
+@endlocal
--- assemblies/geronimo-boilerplate-minimal/src/main/underlay/bin/stop-server.bat
+++ assemblies/geronimo-boilerplate-minimal/src/main/underlay/bin/stop-server.bat
@@ -1,37 +1,82 @@
-@if "%DEBUG%" == "" @echo off
-rem 
-rem Licensed to the Apache Software Foundation (ASF) under one
-rem or more contributor license agreements.  See the NOTICE file
-rem distributed with this work for additional information
-rem regarding copyright ownership.  The ASF licenses this file
-rem to you under the Apache License, Version 2.0 (the
-rem "License"); you may not use this file except in compliance
-rem with the License.  You may obtain a copy of the License at
-rem 
-rem  http://www.apache.org/licenses/LICENSE-2.0
-rem 
-rem Unless required by applicable law or agreed to in writing,
-rem software distributed under the License is distributed on an
-rem "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-rem KIND, either express or implied.  See the License for the
-rem specific language governing permissions and limitations
-rem under the License.
-rem 
+@rem 
+@rem Licensed to the Apache Software Foundation (ASF) under one
+@rem or more contributor license agreements.  See the NOTICE file
+@rem distributed with this work for additional information
+@rem regarding copyright ownership.  The ASF licenses this file
+@rem to you under the Apache License, Version 2.0 (the
+@rem "License"); you may not use this file except in compliance
+@rem with the License.  You may obtain a copy of the License at
+@rem 
+@rem  http://www.apache.org/licenses/LICENSE-2.0
+@rem 
+@rem Unless required by applicable law or agreed to in writing,
+@rem software distributed under the License is distributed on an
+@rem "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+@rem KIND, either express or implied.  See the License for the
+@rem specific language governing permissions and limitations
+@rem under the License.
+@rem 
 
-rem 
-rem $Rev$ $Date$
-rem 
+@rem 
+@rem $Rev$ $Date$
+@rem 
 
-if "%OS%"=="Windows_NT" setlocal
+@rem --------------------------------------------------------------------
+@rem stop-server batch file for Geronimo that stops Geronimo in foreground.
+@rem
+@rem This batch file calls the gsh.bat script passing "geronimo/stop-server
+@rem followed by the arguments supplied by the caller.
+@rem
+@rem Alternatively you can use the more comprehensive gsh.bat file directly.
+@rem
+@rem Invocation Syntax:
+@rem
+@rem   stop-server [geronimo_args ...]
+@rem
+@rem Environment Variable Prequisites:
+@rem
+@rem   Refer to the GShell documentation for information on environment
+@rem   variables etc.
+@rem
+@rem --------------------------------------------------------------------
 
-:begin
+@if "%GERONIMO_BATCH_ECHO%" == "on"  echo on
+@if not "%GERONIMO_BATCH_ECHO%" == "on"  echo off
+
+if "%OS%"=="Windows_NT" goto okOsCheck
+echo Cannot process command - you are running an unsupported operating system.
+set ERRORLEVEL=1
+goto end
+
+:okOsCheck
+@setlocal enableextensions
+@set ERRORLEVEL=0
 
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.\
+cd /d %DIRNAME%
 
-"%DIRNAME%\gsh.bat" -c "geronimo/stop-server %*"
+set EXECUTABLE=%DIRNAME%\gsh.bat
+
+@REM Check that target executable exists
+if exist "%EXECUTABLE%" goto okExec
+echo ERROR - Cannot find required script %EXECUTABLE%
+set ERRORLEVEL=1
+goto end
+
+:okExec
+@REM Get remaining unshifted command line arguments and save them in the
+set CMD_LINE_ARGS=
+:setArgs
+if ""%1""=="""" goto doneSetArgs
+set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
+shift
+goto setArgs
+:doneSetArgs
+
+call "%EXECUTABLE%" -c "geronimo/stop-server %CMD_LINE_ARGS%"
 
 :end
-
-if "%OS%"=="Windows_NT" endlocal
-
+@REM pause the batch file if GERONIMO_BATCH_PAUSE is set to 'on'
+if "%GERONIMO_BATCH_PAUSE%" == "on" pause
+@endlocal
