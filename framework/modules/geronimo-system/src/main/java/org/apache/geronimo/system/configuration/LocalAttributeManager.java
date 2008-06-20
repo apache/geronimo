1251473c9ceefc8d30fb68e3ae1aaa120b4bdad4
Lin Sun <linsun@apache.org>
2008-06-20 13:34:29 +0000

    GERONIMO-4139 Add a comment to config-substitutions.properties to indicate its usage
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/branches/2.1@669889 13f79535-47bb-0310-9956-ffa450edef68

--- framework/modules/geronimo-system/src/main/java/org/apache/geronimo/system/configuration/LocalAttributeManager.java
+++ framework/modules/geronimo-system/src/main/java/org/apache/geronimo/system/configuration/LocalAttributeManager.java
@@ -629,6 +629,15 @@ public class LocalAttributeManager implements LocalPluginAttributeStore, Persist
             try {
                 FileOutputStream out = new FileOutputStream(configSubstitutionsFile);
                 try {
+                    String instruction = "#Put variables and their substitution values in this file: \n"
+                            + "#they will be used when processing config.xml. \n"
+                            + "#Values in this file will be overridden by environment variables and system properties: \n"
+                            + "#    hostName=localhost \n"
+                            + "#    httpPort=8080 \n"
+                            + "#    httpsPort=8443 \n"
+                            + "#To run multiple instances of geronimo choose a portOffset such that none of the ports conflict \n"
+                            + "#Try PortOffset=10 \n";
+                    out.write(instruction.getBytes());                    
                     properties.store(out, null);
                 } finally {
                     out.close();
