33836b2fbada1995af05580b3ad4fb9034140a53
Donald Woods <dwoods@apache.org>
2009-08-04 19:38:42 +0000

    GERONIMO-4758 Remove dependency on SLF4J from 2.1.x version of the fix
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/branches/2.1@800933 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/console/console-filter/src/main/java/org/apache/geronimo/console/filter/FilterResponseWrapper.java
+++ plugins/console/console-filter/src/main/java/org/apache/geronimo/console/filter/FilterResponseWrapper.java
@@ -29,8 +29,8 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 /**
  * Implementation of a HttpServletResponseWrapper to allow us to edit the
@@ -40,7 +40,7 @@ import org.slf4j.LoggerFactory;
  * @version $Rev$ $Date$
  */
 public final class FilterResponseWrapper extends HttpServletResponseWrapper {
-	private static final Logger log = LoggerFactory.getLogger(FilterResponseWrapper.class);
+    // private static final Logger log = LoggerFactory.getLogger(FilterResponseWrapper.class);
     private ByteArrayOutputStream output = null;
     private ResponseOutputStream stream = null;
     private PrintWriter writer = null;
@@ -180,7 +180,7 @@ public final class FilterResponseWrapper extends HttpServletResponseWrapper {
         }
         catch (UnsupportedEncodingException uee) {
             // should never happen
-            log.error("new OutputStreamWriter(stream, UTF-8) failed.", uee);
+            //log.error("new OutputStreamWriter(stream, UTF-8) failed.", uee);
         }
     }
 
