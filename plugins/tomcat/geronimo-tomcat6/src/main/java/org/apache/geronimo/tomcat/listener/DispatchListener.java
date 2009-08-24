1cf50ebb75bdf71939c61edc7222acef6698a515
Haihong Xu <xuhaihong@apache.org>
2009-08-24 14:07:16 +0000

    GERONIMO-4811 The new PolicyContext is not set while forwarding to another web application
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@807225 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/tomcat/geronimo-tomcat6/src/main/java/org/apache/geronimo/tomcat/listener/DispatchListener.java
+++ plugins/tomcat/geronimo-tomcat6/src/main/java/org/apache/geronimo/tomcat/listener/DispatchListener.java
@@ -67,9 +67,12 @@ public class DispatchListener implements InstanceListener {
         BeforeAfter beforeAfter = webContext.getBeforeAfter();
         if (beforeAfter != null) {
             Stack<Object[]> stack = currentContext.get();
-            Object context[] = new Object[webContext.getContextCount() + 1];
+            Object context[] = new Object[webContext.getContextCount() + 2];
             String wrapperName = getWrapperName(request, webContext);
             context[webContext.getContextCount()] = JACCRealm.setRequestWrapperName(wrapperName);
+
+            context[webContext.getContextCount() + 1] = PolicyContext.getContextID();
+            PolicyContext.setContextID(webContext.getPolicyContextId());
 
             beforeAfter.before(context, request, response, BeforeAfter.DISPATCHED);
 
@@ -87,6 +90,7 @@ public class DispatchListener implements InstanceListener {
             beforeAfter.after(context, request, response, BeforeAfter.DISPATCHED);
 
             JACCRealm.setRequestWrapperName((String) context[webContext.getContextCount()]);
+            PolicyContext.setContextID((String) context[webContext.getContextCount() + 1]);
         }
     }
 
