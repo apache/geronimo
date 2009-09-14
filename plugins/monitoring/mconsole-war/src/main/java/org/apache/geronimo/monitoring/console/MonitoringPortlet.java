6b3bfc84a4210d3c2b382505d44a329d1081b2a0
Lin Quan Jiang <genspring@apache.org>
2009-09-14 07:33:29 +0000

    GERONIMO-4854 The protocol option will be reset after testing connection when addint server.  Patch from Du Siqi
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@814505 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/monitoring/mconsole-war/src/main/java/org/apache/geronimo/monitoring/console/MonitoringPortlet.java
+++ plugins/monitoring/mconsole-war/src/main/java/org/apache/geronimo/monitoring/console/MonitoringPortlet.java
@@ -418,6 +418,7 @@ public class MonitoringPortlet extends BasePortlet {
             request.setAttribute("username", request.getParameter("username"));
             request.setAttribute("password", request.getParameter("password"));
             request.setAttribute("password2", request.getParameter("password2"));
+            request.setAttribute("protocol", request.getParameter("protocol"));
             request.setAttribute("port", request.getParameter("port"));
             addServer.include(request, response);
         } else if (action.equals("testEditServerConnection")) {
