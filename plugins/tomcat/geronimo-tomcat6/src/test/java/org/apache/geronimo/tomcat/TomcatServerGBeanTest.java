b1f4e046263898245fb524bf3927527d31e0ec6d
Haihong Xu <xuhaihong@apache.org>
2009-11-20 02:56:24 +0000

    GERONIMO-4953 No way to set Tomcat native cluster on engine and host level
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@882391 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/tomcat/geronimo-tomcat6/src/test/java/org/apache/geronimo/tomcat/TomcatServerGBeanTest.java
+++ plugins/tomcat/geronimo-tomcat6/src/test/java/org/apache/geronimo/tomcat/TomcatServerGBeanTest.java
@@ -17,25 +17,28 @@
  * under the License.
  */
 
-
 package org.apache.geronimo.tomcat;
 
 import java.io.File;
 import java.io.FileReader;
 
-import org.apache.geronimo.testsupport.TestSupport;
-import org.apache.geronimo.tomcat.model.ServerType;
-import org.apache.geronimo.system.serverinfo.ServerInfo;
-import org.apache.geronimo.system.serverinfo.BasicServerInfo;
-import org.apache.catalina.Server;
 import org.apache.catalina.Lifecycle;
+import org.apache.catalina.Server;
+import org.apache.geronimo.system.serverinfo.BasicServerInfo;
+import org.apache.geronimo.system.serverinfo.ServerInfo;
+import org.apache.geronimo.testsupport.TestSupport;
+import org.apache.geronimo.tomcat.model.ChannelType;
+import org.apache.geronimo.tomcat.model.ClusterType;
+import org.apache.geronimo.tomcat.model.ServerType;
 
 /**
  * @version $Rev$ $Date$
  */
-
 public class TomcatServerGBeanTest extends TestSupport {
+
     private static final String SERVER_1 = "src/test/resources/deployables/server-1.xml";
+
+    private static final String SERVER_2 = "src/test/resources/deployables/server-2.xml";
 
     public void testLoadServer1() throws Exception {
         File server1 = resolveFile(SERVER_1);
@@ -45,17 +48,43 @@ public class TomcatServerGBeanTest extends TestSupport {
             assertEquals(4, serverType.getListener().size());
             Server server = serverType.build(getClass().getClassLoader(), null);
             try {
-                ((Lifecycle)server).start();
+                ((Lifecycle) server).start();
             } finally {
-                ((Lifecycle)server).stop();
+                ((Lifecycle) server).stop();
             }
         } finally {
             in.close();
         }
     }
+
+    public void testLoadServer2() throws Exception {
+        File server2 = resolveFile(SERVER_2);
+        FileReader in = new FileReader(server2);
+        try {
+            ServerType serverType = TomcatServerGBean.loadServerType(in);
+            ClusterType engineClusterType = serverType.getService().get(0).getEngine().getCluster();
+            assertNotNull(engineClusterType);
+            validateClusterType(engineClusterType);
+            ClusterType hostClusterType = serverType.getService().get(0).getEngine().getHost().get(0).getCluster();
+            assertNotNull(hostClusterType);
+            validateClusterType(hostClusterType);
+            serverType.build(getClass().getClassLoader(), null);
+        } finally {
+            in.close();
+        }
+    }
+
+    private void validateClusterType(ClusterType clusterType) {
+        assertEquals(2, clusterType.getValve().size());
+        assertEquals(2, clusterType.getClusterListener().size());
+        ChannelType channelType = clusterType.getChannel();
+        assertEquals(3, channelType.getInterceptor().size());        
+        assertEquals(1, channelType.getInterceptor().get(2).getMember().size());
+    }
+
     public void testGBeanServer1() throws Exception {
         ServerInfo serverInfo = new BasicServerInfo(BASEDIR.getAbsolutePath());
-        TomcatServerGBean tomcatServerGBean = new TomcatServerGBean(null, SERVER_1, null ,serverInfo, null, null, getClass().getClassLoader(), null);
+        TomcatServerGBean tomcatServerGBean = new TomcatServerGBean(null, SERVER_1, null, serverInfo, null, null, getClass().getClassLoader(), null);
         try {
             tomcatServerGBean.doStart();
         } finally {
