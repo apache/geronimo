60a2e290ff3a3ec6d1ba8aa79723d5bc45c53120
Jarek Gawor <gawor@apache.org>
2010-01-25 16:47:01 +0000

    GERONIMO-5030: move extender into its own package
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@902869 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/wab/geronimo-web-extender/src/main/java/org/apache/geronimo/osgi/web/extender/WebContainerEventDispatcher.java
+++ plugins/wab/geronimo-web-extender/src/main/java/org/apache/geronimo/osgi/web/extender/WebContainerEventDispatcher.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.geronimo.osgi.web;
+package org.apache.geronimo.osgi.web.extender;
 
 import java.util.Dictionary;
 import java.util.Hashtable;
--- plugins/wab/geronimo-web-extender/src/main/java/org/apache/geronimo/osgi/web/extender/WebContainerExtender.java
+++ plugins/wab/geronimo-web-extender/src/main/java/org/apache/geronimo/osgi/web/extender/WebContainerExtender.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.geronimo.osgi.web;
+package org.apache.geronimo.osgi.web.extender;
 
 import java.util.ArrayList;
 import java.util.Collection;
