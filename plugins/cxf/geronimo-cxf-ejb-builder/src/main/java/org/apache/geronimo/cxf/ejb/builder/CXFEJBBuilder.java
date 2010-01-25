5b1756977674d83e57bffbf72d8f86101e4e1d6c
Lei Wang <rwonly@apache.org>
2010-01-25 09:50:12 +0000

    GERONIMO-5016: 1. make cxf & geronimo-cxf can be compiled 2. refactor the package name in geronimo-cxf-ejb-builder to avoid split package issue.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@902753 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/cxf/geronimo-cxf-ejb-builder/src/main/java/org/apache/geronimo/cxf/ejb/builder/CXFEJBBuilder.java
+++ plugins/cxf/geronimo-cxf-ejb-builder/src/main/java/org/apache/geronimo/cxf/ejb/builder/CXFEJBBuilder.java
@@ -14,7 +14,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.apache.geronimo.cxf.builder;
+package org.apache.geronimo.cxf.ejb.builder;
 
 import java.util.Collection;
 
