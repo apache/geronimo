ae8eb66734f1a987640db8c63a30c5de086283b6
Jarek Gawor <gawor@apache.org>
2010-04-07 15:20:26 +0000

    GERONIMO-5113: Missed a file in previous commit
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@931584 13f79535-47bb-0310-9956-ffa450edef68

---
+++ plugins/connector-1_6/geronimo-connector-1_6/src/main/java/org/apache/geronimo/datasource/DataSourceDescription.java
@@ -0,0 +1,195 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.geronimo.datasource;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * @version $Revision$
+ */
+public class DataSourceDescription implements Serializable {
+    
+    // standard settings
+    private String name;
+    private String className;
+    private String description;
+    private String url;
+    private String user;
+    private String password;
+    private String databaseName;
+    private String serverName;
+    private int portNumber = -1;
+    private int loginTimeout = 0;
+    private Map<String, String> properties;
+    
+    // transaction settings
+    private boolean transactional = true;
+  
+    // pool settings
+    private int initialPoolSize = -1;
+    private int maxPoolSize = -1;
+    private int minPoolSize = -1;
+    private int maxIdleTime = -1;
+    private int maxStatements = -1; 
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+    
+    public void setUser(String user) {
+        this.user = user;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getDatabaseName() {
+        return databaseName;
+    }
+    
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
+    
+    public String getServerName() {
+        return serverName;
+    }
+    
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+    
+    public int getPortNumber() {
+        return portNumber;
+    }
+    
+    public void setPortNumber(int portNumber) {
+        this.portNumber = portNumber;
+    }
+    
+    public int getLoginTimeout() {
+        return loginTimeout;
+    }
+    
+    public void setLoginTimeout(int loginTimeout) {
+        this.loginTimeout = loginTimeout;
+    }
+    
+    public boolean isTransactional() {
+        return transactional;
+    }
+    
+    public void setTransactional(boolean transactional) {
+        this.transactional = transactional;
+    }
+
+    public Map<String, String> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Map<String, String> properties) {
+        this.properties = properties;
+    }
+
+    public int getInitialPoolSize() {
+        return initialPoolSize;
+    }
+
+    public void setInitialPoolSize(int initialPoolSize) {
+        this.initialPoolSize = initialPoolSize;
+    }
+
+    public int getMaxPoolSize() {
+        return maxPoolSize;
+    }
+
+    public void setMaxPoolSize(int maxPoolSize) {
+        this.maxPoolSize = maxPoolSize;
+    }
+
+    public int getMinPoolSize() {
+        return minPoolSize;
+    }
+
+    public void setMinPoolSize(int minPoolSize) {
+        this.minPoolSize = minPoolSize;
+    }
+
+    public int getMaxIdleTime() {
+        return maxIdleTime;
+    }
+
+    public void setMaxIdleTime(int maxIdleTime) {
+        this.maxIdleTime = maxIdleTime;
+    }
+
+    public int getMaxStatements() {
+        return maxStatements;
+    }
+
+    public void setMaxStatements(int maxStatements) {
+        this.maxStatements = maxStatements;
+    }
+    
+    public boolean hasPoolingProperties() {
+        return (maxPoolSize != -1
+                || minPoolSize != -1
+                || maxIdleTime != -1
+                || initialPoolSize != -1
+                || maxStatements != -1);
+    }
+  
+}
