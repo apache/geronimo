e7c83c6932c75743d9d5ffbaf4d4514ce896b17d
Lin Quan Jiang <genspring@apache.org>
2010-04-22 09:18:10 +0000

    GERONIMO-5219 Java EE Injection annotation scanning for 299 beans, make the sample start to work.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@936670 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/openwebbeans/geronimo-openwebbeans-builder/src/main/java/org/apache/geronimo/openwebbeans/deployment/OpenWebBeansModuleBuilderExtension.java
+++ plugins/openwebbeans/geronimo-openwebbeans-builder/src/main/java/org/apache/geronimo/openwebbeans/deployment/OpenWebBeansModuleBuilderExtension.java
@@ -17,14 +17,23 @@
 
 package org.apache.geronimo.openwebbeans.deployment;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.jar.JarFile;
+
+import javax.enterprise.inject.spi.Bean;
+
 import org.apache.geronimo.common.DeploymentException;
 import org.apache.geronimo.deployment.ModuleIDBuilder;
 import org.apache.geronimo.deployment.service.EnvironmentBuilder;
 import org.apache.geronimo.gbean.AbstractName;
-import org.apache.geronimo.gbean.AbstractNameQuery;
 import org.apache.geronimo.gbean.GBeanData;
-import org.apache.geronimo.gbean.GBeanInfo;
-import org.apache.geronimo.gbean.GBeanInfoBuilder;
+import org.apache.geronimo.gbean.annotation.GBean;
 import org.apache.geronimo.gbean.annotation.ParamAttribute;
 import org.apache.geronimo.gbean.annotation.ParamReference;
 import org.apache.geronimo.j2ee.annotation.Holder;
@@ -39,24 +48,18 @@ import org.apache.geronimo.kernel.config.ConfigurationStore;
 import org.apache.geronimo.kernel.repository.Environment;
 import org.apache.geronimo.xbeans.javaee6.FullyQualifiedClassType;
 import org.apache.geronimo.xbeans.javaee6.ListenerType;
-import org.apache.geronimo.xbeans.javaee6.ServletType;
 import org.apache.geronimo.xbeans.javaee6.WebAppType;
+import org.apache.webbeans.servlet.WebBeansConfigurationListener;
 import org.apache.xbean.finder.ClassFinder;
 import org.apache.xmlbeans.XmlObject;
+import org.osgi.framework.Bundle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.jar.JarFile;
 
 /**
  * @version $Rev $Date
  */
+@GBean(j2eeType = NameFactory.MODULE_BUILDER)
 public class OpenWebBeansModuleBuilderExtension implements ModuleBuilderExtension {
 
     private static final Logger log = LoggerFactory.getLogger(OpenWebBeansModuleBuilderExtension.class);
@@ -64,63 +67,81 @@ public class OpenWebBeansModuleBuilderExtension implements ModuleBuilderExtensio
     private final Environment defaultEnvironment;
     private final NamingBuilder namingBuilders;
 
-    private static final String CONTEXT_LISTENER_NAME = "";
+    private static final String CONTEXT_LISTENER_NAME = WebBeansConfigurationListener.class.getName();
 
-    public OpenWebBeansModuleBuilderExtension(Environment defaultEnvironment, NamingBuilder namingBuilders) {
+    public OpenWebBeansModuleBuilderExtension(
+            @ParamAttribute(name = "defaultEnvironment") Environment defaultEnvironment,
+            @ParamReference(name = "NamingBuilders", namingType = NameFactory.MODULE_BUILDER) NamingBuilder namingBuilders) {
         this.defaultEnvironment = defaultEnvironment;
         this.namingBuilders = namingBuilders;
     }
 
-    public void createModule(Module module, Object plan, JarFile moduleFile, String targetPath, URL specDDUrl, Environment environment, Object moduleContextInfo, AbstractName earName, Naming naming, ModuleIDBuilder idBuilder) throws DeploymentException {
+    public void createModule(Module module, Bundle bundle, Naming naming, ModuleIDBuilder idBuilder)
+            throws DeploymentException {
         if (!(module instanceof WebModule)) {
-            //not a web module, nothing to do
+            // not a web module, nothing to do
             return;
         }
 
-        EnvironmentBuilder.mergeEnvironments(environment, defaultEnvironment);
+        EnvironmentBuilder.mergeEnvironments(module.getEnvironment(), defaultEnvironment);
+
     }
 
-    public void installModule(JarFile earFile, EARContext earContext, Module module, Collection configurationStores, ConfigurationStore targetConfigurationStore, Collection repository) throws DeploymentException {
-    }
+    public void createModule(Module module, Object plan, JarFile moduleFile, String targetPath, URL specDDUrl,
+            Environment environment, Object moduleContextInfo, AbstractName earName, Naming naming,
+            ModuleIDBuilder idBuilder) throws DeploymentException {
 
-    public void initContext(EARContext earContext, Module module, ClassLoader cl) throws DeploymentException {
-    }
-
-    public void addGBeans(EARContext earContext, Module module, ClassLoader cl, Collection repository) throws DeploymentException {
         if (!(module instanceof WebModule)) {
-            //not a web module, nothing to do
+            // not a web module, nothing to do
+            return;
+        }
+
+        EnvironmentBuilder.mergeEnvironments(module.getEnvironment(), defaultEnvironment);
+
+    }
+
+    public void installModule(JarFile earFile, EARContext earContext, Module module, Collection configurationStores,
+            ConfigurationStore targetConfigurationStore, Collection repository) throws DeploymentException {
+    }
+
+    public void initContext(EARContext earContext, Module module, Bundle bundle) throws DeploymentException {
+    }
+
+    public void addGBeans(EARContext earContext, Module module, Bundle bundle, Collection repository)
+            throws DeploymentException {
+        if (!(module instanceof WebModule)) {
+            // not a web module, nothing to do
             return;
         }
         WebModule webModule = (WebModule) module;
         WebAppType webApp = (WebAppType) webModule.getSpecDD();
 
-
         EARContext moduleContext = module.getEarContext();
         Map sharedContext = module.getSharedContext();
 
-        //add the ServletContextListener to the web app context
+        // add the ServletContextListener to the web app context
         GBeanData webAppData = (GBeanData) sharedContext.get(WebModule.WEB_APP_DATA);
 
-        //jetty specific support
+        // jetty specific support
         Object value = webAppData.getAttribute("listenerClassNames");
         if (value instanceof Collection && !((Collection) value).contains(CONTEXT_LISTENER_NAME)) {
             ((Collection<String>) value).add(CONTEXT_LISTENER_NAME);
         } else {
-            //try to add listener to the web app xml
+            // try to add listener to the web app xml
             ListenerType listenerType = webApp.addNewListener();
             FullyQualifiedClassType className = listenerType.addNewListenerClass();
             className.setStringValue(CONTEXT_LISTENER_NAME);
         }
 
         AbstractName moduleName = moduleContext.getModuleName();
-        Map<NamingBuilder.Key, Object> buildingContext = new HashMap<NamingBuilder.Key, Object>();
+        Map<EARContext.Key, Object> buildingContext = new HashMap<EARContext.Key, Object>();
         buildingContext.put(NamingBuilder.GBEAN_NAME_KEY, moduleName);
 
-        //use the same jndi context as the web app
+        // use the same jndi context as the web app
         Map compContext = NamingBuilder.JNDI_KEY.get(module.getSharedContext());
         buildingContext.put(NamingBuilder.JNDI_KEY, compContext);
 
-        //use the same holder object as the web app.
+        // use the same holder object as the web app.
         Holder holder = NamingBuilder.INJECTION_KEY.get(sharedContext);
         buildingContext.put(NamingBuilder.INJECTION_KEY, holder);
 
@@ -133,47 +154,65 @@ public class OpenWebBeansModuleBuilderExtension implements ModuleBuilderExtensio
 
     }
 
-    protected ClassFinder createOpenWebBeansClassFinder(WebAppType webApp, WebModule webModule) throws DeploymentException {
+    protected ClassFinder createOpenWebBeansClassFinder(WebAppType webApp, WebModule webModule)
+            throws DeploymentException {
 
         List<Class> classes = getManagedClasses(webApp, webModule);
         return new ClassFinder(classes);
     }
 
-
+    /**
+     * getManagedClasses()
+     * <p/>
+     * <p>
+     * Locations to search for the webbean configuration file(s):
+     * <ol>
+     * <li>META-INF/beans.xml
+     * <li>WEB-INF/beans.xml
+     * </ol>
+     * <p/>
+     * <p>
+     * <strong>Notes:</strong>
+     * <ul>
+     * </ul>
+     * 
+     * @param webApp
+     *            spec DD for module
+     * @param webModule
+     *            module being deployed
+     * @return list of all managed bean classes from all faces-config xml files.
+     * @throws org.apache.geronimo.common.DeploymentException
+     *             if a faces-config.xml file is located but cannot be parsed.
+     */
     private List<Class> getManagedClasses(WebAppType webApp, WebModule webModule) throws DeploymentException {
-        log.debug("getManagedClasses( " + webApp.toString() + "," + '\n' +
-                (webModule != null ? webModule.getName() : null) + " ): Entry");
+        log.debug("getFacesClasses( " + webApp.toString() + "," + '\n'
+                + (webModule != null ? webModule.getName() : null) + " ): Entry");
 
-        // Get the classloader from the module's EARContext
-        ClassLoader classLoader = webModule.getEarContext().getClassLoader();
 
+        Bundle bundle = webModule.getEarContext().getDeploymentBundle();
+        
+        
+
+        // 1. META-INF/beans.xml
         List<Class> classes = new ArrayList<Class>();
-
-        // Look for all META-INF/beans.xml files and process those urls
-
-        // Add the managed bean classes
-
-        log.debug("getManagedClasses() Exit: " + classes.size() + " " + classes.toString());
-
-        // return the managed bean classes
+        
+        WebBeansScanner ws=new WebBeansScanner(bundle);
+        
+        ws.scanWebBeans();
+        
+        Set<Bean<?>> beanSet=ws.getWebBeans();
+        
+        for (Bean<?> bean:beanSet){
+            
+            classes.add(bean.getBeanClass()) ;
+            
+            log.error("-------------------------------bean.getBeanClass()="+bean.getBeanClass());
+            
+        }
+        
         return classes;
     }
 
-    public static final GBeanInfo GBEAN_INFO;
-
-    static {
-        GBeanInfoBuilder infoBuilder = GBeanInfoBuilder.createStatic(OpenWebBeansModuleBuilderExtension.class, NameFactory.MODULE_BUILDER);
-        infoBuilder.addAttribute("defaultEnvironment", Environment.class, true, true);
-        infoBuilder.addReference("NamingBuilders", NamingBuilder.class, NameFactory.MODULE_BUILDER);
-
-        infoBuilder.setConstructor(new String[]{
-                "defaultEnvironment",
-                "NamingBuilders"});
-        GBEAN_INFO = infoBuilder.getBeanInfo();
-    }
-
-    public static GBeanInfo getGBeanInfo() {
-        return GBEAN_INFO;
-    }
+    
 
 }
