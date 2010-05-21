87c59d93edef487a4aba41098b7d79e1fbaa1e31
Jarek Gawor <gawor@apache.org>
2010-05-21 18:16:31 +0000

    simple tests to test eba deployment
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/branches/3.0-M1@947110 13f79535-47bb-0310-9956-ffa450edef68

---
+++ testsuite/aries-testsuite/simple/simple-wab/src/main/java/org/apache/geronimo/test/eba/SampleServlet.java
@@ -0,0 +1,46 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.geronimo.test.eba;
+
+import org.apache.geronimo.test.eba.api.Hello;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class  SampleServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Hello hello = new Hello();
+        
+        PrintWriter out = response.getWriter();
+        out.println("<html><head><title>Sample Wab Application Bundle</title></head></html>");
+        out.println("<body><h1>");
+        out.println(hello.sayHello("Web Application Bundle"));
+        out.println("</h1></body></html>");
+    }
+    
+}
