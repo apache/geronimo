4b0be4695a2df78ac041006ce976d381e531eb74
Ming Xia <xiaming@apache.org>
2010-07-29 09:10:30 +0000

    GERONIMO-5481 committed the new test cases for JSP 2.2 new features provided by Guang Zhe Wang
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@980371 13f79535-47bb-0310-9956-ffa450edef68

---
+++ testsuite/javaee6-testsuite/jsp2.2-test/src/test/java/org/apache/geronimo/testsuite/jsp22/TestJSP.java
@@ -0,0 +1,70 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.apache.geronimo.testsuite.jsp22;
+
+import org.testng.annotations.Test;
+import org.apache.geronimo.testsupport.SeleniumTestSupport;
+
+public class TestJSP extends SeleniumTestSupport {
+
+	@Test
+	public void testWithDefaultContentType() throws Exception {
+		String appContextStr = System.getProperty("appContext");
+		selenium.open(appContextStr);
+		selenium.click("link=Page with contentType.");
+		waitForPageLoad();
+		assertTrue(selenium
+				.isTextPresent("Page with contentType uses tag:default-content-type which value is text/xml."));
+		assertFalse(selenium.isTextPresent("<html>"));
+	}
+
+	// XML page test error in Selenium. This test is excluded at this time.
+	@Test
+	public void testWithoutDefaultContentType() throws Exception {
+		String appContextStr = System.getProperty("appContext");
+		selenium.open(appContextStr);
+		selenium.click("link=Page without contentType.");
+		waitForPageLoad();
+		assertTrue(selenium.isTextPresent("<html>"));
+		assertTrue(selenium
+				.isTextPresent("Page without contentType uses tag: default-content-type which value is text/xml."));
+	}
+
+	@Test
+	public void testWithErrorNamespace() throws Exception {
+		String appContextStr = System.getProperty("appContext");
+		selenium.open(appContextStr);
+		selenium.click("link=Page with tag: error-on-undeclared-namespace = true.");
+		waitForPageLoad();
+		assertTrue(selenium
+				.isTextPresent("A custom tag was encountered with an undeclared namespace [qq]"));
+	}
+
+	@Test
+	public void testWithoutErrorNamespace() throws Exception {
+		String appContextStr = System.getProperty("appContext");
+		selenium.open(appContextStr);
+		selenium.click("link=Page with tag: error-on-undeclared-namespace = false.");
+		waitForPageLoad();
+		assertTrue(selenium
+				.isTextPresent("Page with tag: error-on-undeclared-namespace = false."));
+		assertTrue(selenium.isTextPresent("error namespace value"));
+	}
+}
