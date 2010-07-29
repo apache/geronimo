7b00e2ede9e4659e878fa3ea274718562b3b8d5f
Haihong Xu <xuhaihong@apache.org>
2010-07-29 10:17:29 +0000

    Remove jndi related items from deployment plan to prevent Tomcat handles them.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@980389 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/tomcat/geronimo-tomcat7-builder/src/main/java/org/apache/geronimo/tomcat/deployment/TomcatModuleBuilder.java
+++ plugins/tomcat/geronimo-tomcat7-builder/src/main/java/org/apache/geronimo/tomcat/deployment/TomcatModuleBuilder.java
@@ -264,7 +264,7 @@ public class TomcatModuleBuilder extends AbstractWebModuleBuilder implements GBe
             }
             //else ignore as jee5 allows optional spec dd for .war's
         }
-        
+
         if (webApp == null) {
             webApp = new WebApp();
         }
@@ -591,14 +591,29 @@ public class TomcatModuleBuilder extends AbstractWebModuleBuilder implements GBe
                 webModuleData.setReferencePattern("applicationPolicyConfigurationManager", EARContext.JACC_MANAGER_NAME_KEY.get(earContext.getGeneralData()));
             }
             //not truly metadata complete until MBEs have run
-            if (!webApp.isMetadataComplete()) {
-                webApp.setMetadataComplete(true);
-                if (INITIAL_WEB_XML_SCHEMA_VERSION.get(earContext.getGeneralData()) >= 2.5f) {
-                    String specDeploymentPlan = getSpecDDAsString(webModule);
-                    module.setOriginalSpecDD(specDeploymentPlan);
-                    earContext.addFile(new URI("./WEB-INF/web.xml"), specDeploymentPlan);
-                }
-            }
+            //Those elements have been processed by Geronimo, Tomatt no need to do it now.
+            //Also, currently, it seems that Tomcat could not handle those jndi elements correctly
+            webApp.getEjbLocalRef().clear();
+            webApp.getEjbRef().clear();
+            webApp.getEnvEntry().clear();
+            webApp.getMessageDestination().clear();
+            webApp.getMessageDestinationRef().clear();
+            webApp.getPersistenceContextRef().clear();
+            webApp.getPersistenceUnitRef().clear();
+            webApp.getPostConstruct().clear();
+            webApp.getPreDestroy().clear();
+            webApp.getResourceEnvRef().clear();
+            webApp.getResourceRef().clear();
+            webApp.getServiceRef().clear();
+            webApp.getDataSource().clear();
+            //if (!webApp.isMetadataComplete()) {
+            webApp.setMetadataComplete(true);
+            //if (INITIAL_WEB_XML_SCHEMA_VERSION.get(earContext.getGeneralData()) >= 2.5f) {
+            String specDeploymentPlan = getSpecDDAsString(webModule);
+            module.setOriginalSpecDD(specDeploymentPlan);
+            earContext.addFile(new URI("./WEB-INF/web.xml"), specDeploymentPlan);
+            //}
+            //}
             webModuleData.setAttribute("deploymentDescriptor", module.getOriginalSpecDD());
             module.addAsChildConfiguration();
         } catch (DeploymentException de) {
