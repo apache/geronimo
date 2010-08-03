b36e5793007495aa9132385da52f69f1a2b786f8
Ming Xia <xiaming@apache.org>
2010-08-03 08:45:26 +0000

    GERONIMO-5489 simple test case for JSR 330 support verification in Geronimo, provided by Wang Guang Zhe
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@981779 13f79535-47bb-0310-9956-ffa450edef68

---
+++ testsuite/javaee6-testsuite/di1.0-test/src/main/java/org/apache/geronimo/testsuite/di/app/BigApple.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.geronimo.testsuite.di.app;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+@Singleton
+@Big
+@Named("big")
+public class BigApple extends Apple {
+
+	private static final long serialVersionUID = 1L;
+	private int no = 1;
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void eat() {
+		no--;
+		if (no == 0) {
+			setMessage("Congratulations! You have got the only apple!");
+		} else {
+			setMessage("Sorry, the only apple has been taken by another guy~");
+		}
+	}
+}
---
+++ testsuite/javaee6-testsuite/di1.0-test/src/test/java/org/apache/geronimo/testsuite/di/TestDi.java
@@ -0,0 +1,50 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.apache.geronimo.testsuite.di;
+
+import org.testng.annotations.Test;
+
+import org.apache.geronimo.testsupport.SeleniumTestSupport;
+
+public class TestDi extends SeleniumTestSupport {
+
+	@Test
+	public void testDi() throws Exception {
+		String appContextStr = System.getProperty("appContext");
+		selenium.open(appContextStr);
+		selenium.click("q:q");
+		waitForPageLoad();
+		assertTrue(selenium
+				.isTextPresent("Congratulations! You have got the only apple!"));
+		selenium.click("q:q");
+		waitForPageLoad();
+		selenium.click("q:q");
+		waitForPageLoad();
+		assertTrue(selenium
+				.isTextPresent("Sorry, the only apple has been taken by another guy~"));
+		selenium.click("q:q");
+		waitForPageLoad();
+		selenium.click("q:qq");
+		waitForPageLoad();
+		assertTrue(selenium
+				.isTextPresent("Congratulations! There are enough small apples!"));
+	}
+
+}
