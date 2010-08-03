dff5d49b949d2d0245ce35911eda487f3856de05
Ming Xia <xiaming@apache.org>
2010-08-03 08:29:25 +0000

    GERONIMO-5498 simple test case for managed bean feature of java ee 6 provided by janel Zhang
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@981775 13f79535-47bb-0310-9956-ffa450edef68

---
+++ testsuite/javaee6-testsuite/managedbean1.0-test/src/main/java/org/apache/geronimo/sample/servlet/ManagedBeanJNDIServlet.java
@@ -0,0 +1,74 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.apache.geronimo.sample.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.geronimo.sample.managedbean.MyManagedBean;
+
+/**
+ * Servlet implementation class ManagedBeanServlet
+ */
+public class ManagedBeanJNDIServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ManagedBeanJNDIServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		   MyManagedBean myManagedBean=null;
+		   try {
+				InitialContext ctx=new InitialContext();
+				myManagedBean=(MyManagedBean) ctx.lookup("java:module/mybean");
+			} catch (NamingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        
+		    PrintWriter out = response.getWriter();
+	        out.println("<html><head><title>Common Annotation ManagedBean</title></head></html>");
+	        out.println("<body>");
+            out.println(myManagedBean.saySomeThing("Hello,I am a ManagedBean!"));
+            out.println("</body></html>");
+	}
+
+}
