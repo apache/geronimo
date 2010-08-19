3c8e5b29fc5ffd4802a9abb50effdf90d28de462
Ming Xia <xiaming@apache.org>
2010-08-19 09:01:05 +0000

    GERONIMO-5538 add two test bucket about scope annotation in web bean and webbean+EJB, provided by Lu Jiang
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@987088 13f79535-47bb-0310-9956-ffa450edef68

---
+++ testsuite/javaee6-testsuite/webbean1.0-test/conversation-test/src/test/java/org/apache/webbeans/TestConversation.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.webbeans;
+
+import junit.framework.Assert;
+
+import org.testng.annotations.Test;
+import org.apache.geronimo.testsupport.SeleniumTestSupport;
+
+
+public class TestConversation extends SeleniumTestSupport {
+
+	//test non-conversational context
+	@Test
+	public void testNonConversation(){
+        String appContextStr = System.getProperty("appContext");
+		selenium.open(appContextStr+"/buy.jsf");
+//		selenium.open("/conversation-test/buy.jsf");	
+		selenium.click("//a[@onclick=\"mojarra.jsfcljs(document.getElementById('form'),{'form:j_idt8:1:j_idt15':'form:j_idt8:1:j_idt15'},'');return false\"]");
+		selenium.waitForPageToLoad("30000");		
+		Assert.assertEquals(selenium.getText("xpath=/html/body/form/div[4]/table/tbody/tr[2]/td"),"Item-2");
+		Assert.assertEquals(selenium.getText("xpath=/html/body/form/div[4]/table/tbody/tr[2]/td[2]"),"3000");
+		selenium.click("//a[@onclick=\"mojarra.jsfcljs(document.getElementById('form'),{'form:j_idt8:3:j_idt15':'form:j_idt8:3:j_idt15'},'');return false\"]");
+		selenium.waitForPageToLoad("30000");		
+		Assert.assertEquals(selenium.getText("xpath=/html/body/form/div[4]/table/tbody/tr[2]/td"),"Item-4");
+		Assert.assertEquals(selenium.getText("xpath=/html/body/form/div[4]/table/tbody/tr[2]/td[2]"),"6000");
+		
+	}
+	//test start of the conversation
+	@Test(dependsOnMethods={"testNonConversation"})
+	public void testStartShopping(){
+		Assert.assertEquals(selenium.getText("xpath=//*[@id=\"form:conversation\"]"),"");
+		selenium.click("form:button1");
+		selenium.waitForPageToLoad("30000");
+		Assert.assertNotSame(selenium.getText("xpath=//*[@id=\"form:conversation\"]"), "");	
+	}
+	@Test(dependsOnMethods={"testStartShopping"})
+	public void buyItemTwo() {		
+		selenium.click("//a[@onclick=\"mojarra.jsfcljs(document.getElementById('form'),{'form:j_idt8:1:j_idt15':'form:j_idt8:1:j_idt15'},'');return false\"]");
+		selenium.waitForPageToLoad("30000");		
+		Assert.assertEquals(selenium.getText("xpath=/html/body/form/div[4]/table/tbody/tr[2]/td"),"Item-2");
+		Assert.assertEquals(selenium.getText("xpath=/html/body/form/div[4]/table/tbody/tr[2]/td[2]"),"3000");
+				
+	}
+    //test that data in the conversation is not lost
+	@Test(dependsOnMethods={"buyItemTwo"})
+	public void buyItemFour() {
+		//buy item-4
+		selenium.click("//a[@onclick=\"mojarra.jsfcljs(document.getElementById('form'),{'form:j_idt8:3:j_idt15':'form:j_idt8:3:j_idt15'},'');return false\"]");
+		selenium.waitForPageToLoad("30000");	
+		Assert.assertEquals(selenium.getText("xpath=/html/body/form/div[4]/table/tbody/tr[2]/td"),"Item-2");
+		Assert.assertEquals(selenium.getText("xpath=/html/body/form/div[4]/table/tbody/tr[2]/td[2]"),"3000");
+		Assert.assertEquals(selenium.getText("xpath=/html/body/form/div[4]/table/tbody/tr[3]/td"),"Item-4");
+		Assert.assertEquals(selenium.getText("xpath=/html/body/form/div[4]/table/tbody/tr[3]/td[2]"),"6000");
+	}
+	//test checkout
+	@Test(dependsOnMethods={"buyItemFour"})
+	public void checkOut(){
+		selenium.click("form:button4");
+		selenium.waitForPageToLoad("30000");
+		Assert.assertEquals(selenium.getText("xpath=//*[@id=\"form:conversation\"]"),"");
+		
+	}
+}
---
+++ testsuite/javaee6-testsuite/webbean1.0-test/webbean-ejb-test/src/test/java/org/apache/webbean/sample/ejbtelephone/test/EjbtelephoneTest.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.webbean.sample.ejbtelephone.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.apache.geronimo.testsupport.SeleniumTestSupport;
+
+public class EjbtelephoneTest extends SeleniumTestSupport {
+	@Test
+	public void inputNothing() {
+        String appContextStr = System.getProperty("appContext");
+		selenium.open(appContextStr);
+//		selenium.open("/webbean-ejb-test");
+		selenium.click("form:j_idt16");
+		selenium.waitForPageToLoad("30000");
+		Assert.assertEquals(selenium.getText("xpath=/html/body/div/div/ul/li"),
+		"Please give a name!");
+		Assert.assertEquals(selenium.getText("xpath=/html/body/div/div/ul/li[2]"),
+		"Please give a surname!");
+		Assert.assertEquals(selenium.getText("xpath=/html/body/div/div/ul/li[3]"),
+		"Please give a telephone!");	 	 
+		  
+	}
+
+	@Test(dependsOnMethods = { "inputNothing" })
+	public void saveTelephone() {
+		selenium.type("form:text", "gero");
+		selenium.type("form:surname", "nimo");
+		selenium.type("form:telephone", "12345678");
+		selenium.click("form:j_idt16");
+		selenium.waitForPageToLoad("30000");
+		Assert.assertEquals(selenium.getText("xpath=/html/body/div/div/ul/li"),
+				"Record added");
+	}
+
+	@Test(dependsOnMethods = { "saveTelephone" })
+	public void showAllTelephone() {
+		selenium.click("form:j_idt17");
+		selenium.waitForPageToLoad("30000");
+		Assert.assertEquals(
+						selenium.getText("xpath=/html/body/div/div/form/div[2]/table/tbody/tr/td[2]"),
+						"gero");
+		Assert.assertEquals(
+						selenium.getText("xpath=/html/body/div/div/form/div[2]/table/tbody/tr/td[3]"),
+						"nimo");
+		Assert.assertEquals(
+						selenium.getText("xpath=/html/body/div/div/form/div[2]/table/tbody/tr/td[4]"),
+						"12345678");
+
+	}
+}
