15dcf3f521ee9ac28092560a4895b5d65384b9ee
Ming Xia <xiaming@apache.org>
2010-08-20 08:31:45 +0000

    GERONIMO-5538 add test for webbean+servlet, provided by Lu Jiang
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@987403 13f79535-47bb-0310-9956-ffa450edef68

---
+++ testsuite/javaee6-testsuite/webbean1.0-test/webbean-servlet-test/src/test/java/org/apache/webbean/sample/servlet/test/TestWebbeanServlet.java
@@ -0,0 +1,43 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.apache.webbean.sample.servlet.test;
+
+import org.testng.annotations.Test;
+import org.apache.geronimo.testsupport.SeleniumTestSupport;
+
+public class TestWebbeanServlet extends SeleniumTestSupport {
+
+	@Test
+	public void testCallWebbeansInServlet() throws Exception {
+		String appContextStr = System.getProperty("appContext");
+		selenium.open(appContextStr);
+		selenium.click("link=Test calling webbeans from servelet");
+		waitForPageLoad();
+		assertTrue(selenium
+				.isTextPresent("Injection of Bean Instance into Servlet"));
+		assertTrue(selenium.isTextPresent("Caller Principal name injection into DateProvider instance : ANONYMOUS"));
+		assertTrue(selenium
+				.isTextPresent("Current Date : org.apache.webbeans.samples.tomcat.CurrentDateProvider"));
+		assertTrue(selenium
+				.isTextPresent("Injection of BeanManager into Servlet"));
+		assertTrue(selenium
+				.isTextPresent("Injection of @Inject BeanManager into servlet is successfull"));
+	}
+}
