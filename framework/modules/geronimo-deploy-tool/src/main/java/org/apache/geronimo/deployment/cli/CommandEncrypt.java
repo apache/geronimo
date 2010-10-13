apache/geronimo

commit 12dc3dc4f28ba3ea2afd64be763cd33b98ab0e83

GERONIMO-5638 Remove the encrypt(String) method from ConfiguredEncryption
git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@1022033 13f79535-47bb-0310-9956-ffa450edef68

framework/modules/geronimo-deploy-tool/src/main/java/org/apache/geronimo/deployment/cli/CommandEncrypt.java
@@ -44,7 +44,7 @@ public class CommandEncrypt extends AbstractCommand {
             if (!isOffline(connection)) {
                 // Online encryption            
                 Kernel k = getKernel(connection);
-                Object ret = k.invoke(ConfiguredEncryption.class, "encrypt", new Object[] {commandArgs.getArgs()[0]}, new String[] {"java.lang.String"});
+                Object ret = k.invoke(EncryptionManagerWrapperGBean.class, "encrypt", new Object[] {commandArgs.getArgs()[0]}, new String[] {"java.io.Serializable"});
                 consoleReader.printString(DeployUtils.reformat("Online encryption result: "+ret, 4, 72));
             } else {
                 // Offline encryption

framework/configs/rmi-naming/src/main/plan/plan.xml
@@ -58,10 +58,7 @@
         <attribute name="poolName">DefaultThreadPool</attribute>
     </gbean>
 
-    <gbean name="ConfiguredEncryption" class="org.apache.geronimo.system.util.ConfiguredEncryption">
-        <reference name="ServerInfo">
-            <name>ServerInfo</name>
-        </reference>
+    <gbean name="EncryptionManagerWrapper" class="org.apache.geronimo.system.util.EncryptionManagerWrapperGBean">
     </gbean>
 
 </module>

framework/modules/geronimo-crypto/src/main/java/org/apache/geronimo/crypto/EncryptionManager.java
@@ -49,7 +49,7 @@ public class EncryptionManager {
         ENCRYPTORS.put("{Standard}", SimpleEncryption.INSTANCE);
     }
 
-    private static String encryptionPrefix = SIMPLE_ENCRYPTION_PREFIX;
+    private static String activeEncryptionPrefix = SIMPLE_ENCRYPTION_PREFIX;
 
     /**
      * Encryption instances should call this to register themselves.
@@ -58,8 +58,8 @@ public class EncryptionManager {
      * @param encryption Encryption instance to do the work.
      */
     public synchronized static void setEncryptionPrefix(String prefix, Encryption encryption) {
-        if (SIMPLE_ENCRYPTION_PREFIX.equals(encryptionPrefix)) {
-            encryptionPrefix = prefix;
+        if (activeEncryptionPrefix.equals(SIMPLE_ENCRYPTION_PREFIX)) {  //only can be set once?
+            activeEncryptionPrefix = prefix;
         }
         ENCRYPTORS.put(prefix, encryption);
     }
@@ -74,14 +74,14 @@ public class EncryptionManager {
     public static String encrypt(Serializable source) {
         if (source instanceof String) {
             String sourceString = (String) source;
-            if (sourceString.startsWith(encryptionPrefix)) {
+            if (sourceString.startsWith(activeEncryptionPrefix)) {
                 return (String) source;
             } else if (sourceString.startsWith("{")) {
                 source = decrypt(sourceString);
             }
         }
-        Encryption encryption = ENCRYPTORS.get(encryptionPrefix);
-        return encryptionPrefix + encryption.encrypt(source);
+        Encryption activeEncryption = ENCRYPTORS.get(activeEncryptionPrefix);
+        return activeEncryptionPrefix + activeEncryption.encrypt(source);
     }
 
     /**

framework/modules/geronimo-system/src/main/java/org/apache/geronimo/system/util/ConfiguredEncryption.java
@@ -56,12 +56,7 @@ public class ConfiguredEncryption extends AbstractEncryption implements GBeanLif
 
     private final SecretKeySpec spec;
 
-
     public ConfiguredEncryption(String path, ServerInfo serverInfo) throws IOException, ClassNotFoundException {
-        if (path == null || "".equals(path)) {
-            spec = null;
-            return;
-        }
         File location = serverInfo.resolve(path);
         if (location.exists()) {
             FileInputStream in = new FileInputStream(location);
@@ -104,9 +99,7 @@ public class ConfiguredEncryption extends AbstractEncryption implements GBeanLif
     }
 
     public void doStart() throws Exception {
-        if (spec != null) {
-            EncryptionManager.setEncryptionPrefix("{Configured}", this);
-        }
+        EncryptionManager.setEncryptionPrefix("{Configured}", this);
     }
 
     public void doStop() throws Exception {
@@ -119,10 +112,6 @@ public class ConfiguredEncryption extends AbstractEncryption implements GBeanLif
         return spec;
     }
     
-    public String encrypt(String text) {
-        return EncryptionManager.encrypt(text);
-    }
-
     public static final GBeanInfo GBEAN_INFO;
 
     static {
@@ -130,7 +119,6 @@ public class ConfiguredEncryption extends AbstractEncryption implements GBeanLif
         infoBuilder.addAttribute("path", String.class, true, true);
         infoBuilder.addReference("ServerInfo", ServerInfo.class, "GBean");
         infoBuilder.setConstructor(new String[]{"path", "ServerInfo"});
-        infoBuilder.addOperation("encrypt", new Class[] {String.class}, "java.lang.String");
         GBEAN_INFO = infoBuilder.getBeanInfo();
     }
 

framework/modules/geronimo-system/src/main/java/org/apache/geronimo/system/util/EncryptionManagerWrapperGBean.java
@@ -1,0 +1,33 @@
+package org.apache.geronimo.system.util;
+
+import java.io.Serializable;
+
+import org.apache.geronimo.crypto.EncryptionManager;
+import org.apache.geronimo.gbean.GBeanInfo;
+import org.apache.geronimo.gbean.GBeanInfoBuilder;
+
+
+public class EncryptionManagerWrapperGBean {
+
+    public String encrypt(Serializable source) {
+        return EncryptionManager.encrypt(source);
+    }
+
+    public Serializable decrypt(String source) {
+        return EncryptionManager.decrypt(source);
+    }
+
+    public static final GBeanInfo GBEAN_INFO;
+
+    static {
+        GBeanInfoBuilder infoBuilder = GBeanInfoBuilder.createStatic(EncryptionManagerWrapperGBean.class, "GBean");
+        infoBuilder.addOperation("encrypt", new Class[] {Serializable.class}, "java.io.Serializable");
+        infoBuilder.addOperation("decrypt", new Class[] {String.class}, "java.lang.String");
+        GBEAN_INFO = infoBuilder.getBeanInfo();
+    }
+
+    public static GBeanInfo getGBeanInfo() {
+        return GBEAN_INFO;
+    }
+    
+}

commit 4329f9196b803c578835db819336d6bce9c08834

fix build break caused by r1022033
git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@1022066 13f79535-47bb-0310-9956-ffa450edef68

framework/modules/geronimo-deploy-tool/src/main/java/org/apache/geronimo/deployment/cli/CommandEncrypt.java
@@ -22,6 +22,7 @@ import org.apache.geronimo.common.DeploymentException;
 import org.apache.geronimo.crypto.EncryptionManager;
 import org.apache.geronimo.kernel.Kernel;
 import org.apache.geronimo.system.util.ConfiguredEncryption;
+import org.apache.geronimo.system.util.EncryptionManagerWrapperGBean; 
 
 /**
  * The CLI command to encrypt a given string.