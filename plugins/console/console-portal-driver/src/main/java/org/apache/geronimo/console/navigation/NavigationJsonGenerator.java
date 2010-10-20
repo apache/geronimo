3d2424bd2f7319f7e4e1b06147eb3b677ca2f591
Lin Quan Jiang <genspring@apache.org>
2010-10-20 14:58:44 +0000

    GERONIMO-5612  Filter no permission functions for built-in user "monitor"
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@1025609 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/console/console-portal-driver/src/main/java/org/apache/geronimo/console/navigation/NavigationJsonGenerator.java
+++ plugins/console/console-portal-driver/src/main/java/org/apache/geronimo/console/navigation/NavigationJsonGenerator.java
@@ -16,12 +16,16 @@
  */
 package org.apache.geronimo.console.navigation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.TreeMap;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.geronimo.pluto.impl.PageConfig;
 import org.slf4j.Logger;
@@ -52,6 +56,51 @@ public class NavigationJsonGenerator {
     private static  Map<String, Map<String, TreeNode>> navigationTrees=new HashMap<String, Map<String, TreeNode>>();
     
     public static final String ALL = "all";
+    
+    private final static List<String> monitorRolePermitPages = Arrays.asList(
+            "0/Welcome",
+            "1-1-1/Application Server/System Information/Server Information",
+            "1-1-2/Application Server/System Information/Java System Info",
+            "1-1-4/Application Server/System Information/Thread Pools",
+            "1-8/Server/Monitoring"
+    );
+
+    
+    private static Map<String, List<String>> rolePagesMap=new HashMap<String, List<String>>();
+    
+    static {
+        
+        rolePagesMap.put("monitor", monitorRolePermitPages);
+        
+    }
+    
+    public static List<PageConfig> filterPagesByRole(List<PageConfig> pageConfigList, HttpServletRequest request){
+        
+        List<String> permitPages=null;
+        
+        if(request.isUserInRole("monitor")){
+            
+            permitPages=rolePagesMap.get("monitor"); 
+        }
+        
+        if (permitPages == null) {
+            return pageConfigList;
+        }
+
+        List<PageConfig> filteredPageConfigList = new ArrayList<PageConfig>();
+
+        for (PageConfig pc : pageConfigList) {
+
+            if (permitPages.contains(pc.getName())) {
+
+                filteredPageConfigList.add(pc);
+            }
+        }
+        
+        return filteredPageConfigList;
+        
+    };
+    
       
     private ResourceBundle navigationResourcebundle;
          
