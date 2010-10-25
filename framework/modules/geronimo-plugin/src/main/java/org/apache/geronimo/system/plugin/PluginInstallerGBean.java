d254db6ef5ce30d1daa6f17573cc86d072893e9f
Lin Quan Jiang <genspring@apache.org>
2010-10-25 09:25:18 +0000

    GERONIMO-5637 deploy install-plugin command does not report failure when plugin is failed to install, Thanks Viola for the patch.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@1026994 13f79535-47bb-0310-9956-ffa450edef68

--- framework/modules/geronimo-plugin/src/main/java/org/apache/geronimo/system/plugin/PluginInstallerGBean.java
+++ framework/modules/geronimo-plugin/src/main/java/org/apache/geronimo/system/plugin/PluginInstallerGBean.java
@@ -805,6 +805,7 @@ public class PluginInstallerGBean implements PluginInstaller {
                                     configManager.loadConfiguration(artifact);
                                 } catch (Exception e) {
                                     log.error("Unable to load configuration. ", e);
+                                    poller.setFailure(e);
                                     configManager.uninstallConfiguration(artifact);
                                 }
                             }
