4040d647c0848bbc78aef550c662cb5584aa351b
Haihong Xu <xuhaihong@apache.org>
2010-11-08 06:07:23 +0000

    GERONIMO-5613 Check whether current connection is of OfflineServerConnection to determine whether the embedded mode is used.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@1032466 13f79535-47bb-0310-9956-ffa450edef68

--- framework/modules/geronimo-shell-base/src/main/java/org/apache/geronimo/shell/deploy/LoginCommand.java
+++ framework/modules/geronimo-shell-base/src/main/java/org/apache/geronimo/shell/deploy/LoginCommand.java
@@ -23,6 +23,7 @@ import org.apache.felix.gogo.commands.Command;
 import org.apache.geronimo.cli.deployer.BaseCommandArgs;
 import org.apache.geronimo.deployment.cli.AbstractCommand;
 import org.apache.geronimo.deployment.cli.CommandLogin;
+import org.apache.geronimo.deployment.cli.OfflineServerConnection;
 import org.apache.geronimo.deployment.cli.ServerConnection;
 
 /**
@@ -33,12 +34,13 @@ public class LoginCommand extends ConnectCommand {
 
     @Override
     protected Object doExecute() throws Exception {
-        if (isEmbedded()) {
+
+        ServerConnection connection = connect();
+
+        if (connection instanceof OfflineServerConnection) {
             println("No credentials to save in embedded mode.");
             return null;
         }
-        
-        ServerConnection connection = connect();
 
         AbstractCommand command = new CommandLogin();
 
