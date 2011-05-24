89661429f69b1c44c0176be028a078dbed953ba5
Jarek Gawor <gawor@apache.org>
2011-05-24 19:03:03 +0000

    GERONIMO-5938: Tests for loading jsf taglibs from wired bundles
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@1127222 13f79535-47bb-0310-9956-ffa450edef68

---
+++ testsuite/aries-testsuite/jsf/jsf-taglib/src/main/java/org/apache/geronimo/test/jsf/taglib/DateTag.java
@@ -0,0 +1,47 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.geronimo.test.jsf.taglib;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.faces.component.UIComponentBase;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+
+public class DateTag extends UIComponentBase {
+
+    public DateTag() {
+    }
+    
+    @Override
+    public void encodeBegin(FacesContext context) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+
+        String type = (String) getAttributes().get("type");
+
+        writer.startElement(type, this);
+        writer.writeText("Today is: " + new Date(), null);
+        writer.endElement(type);
+    }
+
+    @Override
+    public String getFamily() {
+        return "Date";
+    }
+
+}
