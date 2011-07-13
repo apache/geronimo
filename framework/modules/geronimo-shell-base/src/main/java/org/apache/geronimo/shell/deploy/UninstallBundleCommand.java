b786b55d37fae541fb709b649a173cbeec05b7fc
Runhua Chi <chirunhua@apache.org>
2011-07-13 15:08:50 +0000

    support bundle installation and uninstallation from Karaf Shell
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@1146091 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/modules/geronimo-shell-base/src/main/java/org/apache/geronimo/shell/deploy/UninstallBundleCommand.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.geronimo.shell.deploy;
+
+import java.util.Arrays;
+
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.Command;
+import org.apache.geronimo.cli.deployer.BaseCommandArgs;
+import org.apache.geronimo.deployment.cli.AbstractCommand;
+import org.apache.geronimo.deployment.cli.CommandUninstallBundle;
+import org.apache.geronimo.deployment.cli.ServerConnection;
+
+/**
+ * @version $Rev$ $Date$
+ */
+@Command(scope = "deploy", name = "uninstall-bundle", description = "Uninstall an OSGi bundle")
+public class UninstallBundleCommand extends ConnectCommand {
+
+    @Argument(required = true, description = "Bundle Id")
+    String bundleId;
+
+    @Override
+    protected Object doExecute() throws Exception {
+        ServerConnection connection = connect();
+
+        AbstractCommand command = new CommandUninstallBundle();
+
+        BaseCommandArgs args = new BaseCommandArgs(new String[]{bundleId});
+
+        command.execute(this, connection, args);
+        return null;
+    }
+
+}
+
+
+  
+     
+
+
