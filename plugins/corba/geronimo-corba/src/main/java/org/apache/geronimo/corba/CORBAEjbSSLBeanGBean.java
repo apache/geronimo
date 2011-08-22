abddc203f2e50b08cf64efcc8ab54448782f183f
Hong Fang Han <hanhongfang@apache.org>
2011-08-22 09:14:49 +0000

    GERONIMO-6116 differentiate the two "OpenEJB ORB Adapter" displayed in "Linstening on Ports" after server startup.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/trunk@1160155 13f79535-47bb-0310-9956-ffa450edef68

---
+++ plugins/corba/geronimo-corba/src/main/java/org/apache/geronimo/corba/CORBAEjbSSLBeanGBean.java
@@ -0,0 +1,45 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.geronimo.corba;
+
+import org.apache.geronimo.gbean.GBeanInfo;
+import org.apache.geronimo.gbean.GBeanInfoBuilder;
+import org.apache.geronimo.j2ee.j2eeobjectnames.NameFactory;
+import org.apache.geronimo.openejb.OpenEjbSystem;
+
+/**
+ * 
+ *
+ * @version $Rev$ $Date$
+ */
+public class CORBAEjbSSLBeanGBean {
+    public static final GBeanInfo GBEAN_INFO;
+
+    static {
+        GBeanInfoBuilder infoBuilder = GBeanInfoBuilder.createStatic(CORBAEjbSSLBeanGBean.class, "OpenEJB SSL ORB Adapter", CORBAEjbBean.class, CORBABeanGBean.GBEAN_INFO, NameFactory.CORBA_SERVICE);
+
+        infoBuilder.addReference("OpenEjbSystem", OpenEjbSystem.class);
+
+        infoBuilder.setConstructor(new String[]{"abstractName", "ConfigAdapter", "host", "port", "classLoader", "NameService", "OpenEjbSystem", "SSLConfig"});
+
+        GBEAN_INFO = infoBuilder.getBeanInfo();
+    }
+
+    public static GBeanInfo getGBeanInfo() {
+        return GBEAN_INFO;
+    }
+}
