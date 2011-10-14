3be17b2a603ca34f1c8bbfb783a3c4b9f3374238
Dan Lu <violalu@apache.org>
2011-10-14 07:17:55 +0000

    GERONIMO-6187 filter keystore files under var/security/keystores
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/server/branches/2.1@1183228 13f79535-47bb-0310-9956-ffa450edef68

--- framework/modules/geronimo-security/src/main/java/org/apache/geronimo/security/keystore/FileKeystoreManager.java
+++ framework/modules/geronimo-security/src/main/java/org/apache/geronimo/security/keystore/FileKeystoreManager.java
@@ -123,12 +123,31 @@ public class FileKeystoreManager implements KeystoreManager, GBeanLifecycle {
     }
     
     public String[] listKeystoreFiles() {
-        File[] files = directory.listFiles();
+    	File[] files = directory.listFiles();
         List list = new ArrayList();
         for (int i = 0; i < files.length; i++) {
             File file = files[i];
             if(file.canRead() && !file.isDirectory()) {
-                list.add(file.getName());
+                String name = file.getName();
+                if (name.lastIndexOf(".") == -1) {
+                    list.add(file.getName());
+                } else {
+                    String type = name.substring(name.lastIndexOf(".") + 1);    
+                    if (file.length()> 0){
+                        for(String ktype : KeystoreUtil.keystoreTypes ){
+                            if (ktype.toLowerCase().equals(type.toLowerCase())){                                
+                                list.add(file.getName());
+                            }
+                        }
+                    } else if (file.length() == 0){
+                        for (String ktype : KeystoreUtil.emptyKeystoreTypes){
+                            if (ktype.toLowerCase().equals(type.toLowerCase())){                        
+                                list.add(file.getName());
+                            }
+                        }
+                    }
+                }
+                            
             }
         }
         return (String[]) list.toArray(new String[list.size()]);
